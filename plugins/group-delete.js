d310119cd82ce3501dec2c6c1d76dabc8949f04a
Ryzn <76462435+ShirokamiRyzen@users.noreply.github.com>
2022-12-03 21:19:21 +0700

    Add files via upload

---
+++ plugins/group-delete.js
@@ -0,0 +1,20 @@
+
+let handler = function (m) {
+ let key = {}
+ try {
+ 	key.remoteJid = m.quoted ? m.quoted.fakeObj.key.remoteJid : m.key.remoteJid
+	key.fromMe = m.quoted ? m.quoted.fakeObj.key.fromMe : m.key.fromMe
+	key.id = m.quoted ? m.quoted.fakeObj.key.id : m.key.id
+ 	key.participant = m.quoted ? m.quoted.fakeObj.participant : m.key.participant
+ } catch (e) {
+ 	console.error(e)
+ }
+ conn.sendMessage(m.chat, { delete: key })
+}
+handler.help = ['delete']
+handler.tags = ['info']
+handler.command = /^(del|delete|unsend?)$/i
+handler.limit = false
+handler.admin = true
+
+export default handler

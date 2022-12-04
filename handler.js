98e273de750a181ae9f9f8712a8e3f08ba89564f
Shirokami Ryzen <infinixgaming360@gmail.com>
2022-12-04 21:44:39 +0700

    modified:   handler.js

--- handler.js
+++ handler.js
@@ -714,13 +714,7 @@ export async function participantsUpdate({ id, participants, action }) {
     sourceUrl: sgc
      }}
   })*/
-  let welcom = 'https://telegra.ph/file/35f17bb371d308504bc46.jpg'
-
-  let godbye = 'https://telegra.ph/file/b44e48066aed4fb7ad291.jpg'
   conn.sendButtonImg(id, await(await fetch(action === 'add' ? wel : lea)).buffer(), 'Group Messege', text, action == 'add' ? 'ᴡᴇʟᴄᴏᴍᴇ' : 'sᴀʏᴏɴᴀʀᴀᴀ', action === 'add' ? '.intro' : 'Aʟᴅɪ X Aɪsʏᴀʜ', fkontak, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: 'https://instagram.com',
-    mediaType: 2, 
-    description: sgc,
     title: "Jᴏɪɴ Sɪɴɪ Cᴜʏ",
     body: wm,
     thumbnail: await(await fetch(action === 'add' ? welcom : godbye)).buffer(),

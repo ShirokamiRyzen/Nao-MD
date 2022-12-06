23edcba47ebdb0eb8527150ab411757034af8b4e
Shirokami Ryzen <infinixgaming360@gmail.com>
2022-12-06 13:32:28 +0700

    modified:   plugins/internet-darkjokes.js

--- plugins/internet-darkjokes.js
+++ plugins/internet-darkjokes.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 import bo from 'dhn-api'
 let handler = async (m, { conn }) => {
 const res = await bo.Darkjokes()
-await conn.sendButton(m.chat,`Dark ga si adick adick`, wm, res, [['Darkjoke','.darkjoke']] ,m)
+await conn.sendButton(m.chat,`Dark banget kayak Negro`, wm, res, [['Darkjoke','.darkjoke']] ,m)
 }
 handler.help = ['darkjoke']
 handler.tags = ['internet']

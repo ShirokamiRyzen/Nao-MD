858689b4bdc04df12fa8f1f32ca68dd802e63985
Shirokami Ryzen <infinixgaming360@gmail.com>
2022-12-08 10:50:46 +0700

    modified:   plugins/_autosticker.js

--- plugins/_autosticker.js
+++ plugins/_autosticker.js
@@ -12,7 +12,7 @@ let stiker = await sticker(null, global.API(`${pickRandom(stikerhuuu)}`), global
           showAdAttribution: true,
     mediaUrl: sgc,
     mediaType: 2,
-    description: '𝑨𝒍𝒅𝒊 𝑿 𝑨𝒊𝒔𝒚𝒂𝒉', 
+    description: 'Shirokami Ryzen', 
     title: 'Hai, ' + name + ' ' + ucapan,
     body: botdate,
     thumbnail: await(await fetch(pp)).buffer(),

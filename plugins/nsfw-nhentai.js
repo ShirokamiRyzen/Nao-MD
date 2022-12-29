d1b17e7af7421aac2f229ef04debc47302769fe8
Shirokami Ryzen <infinixgaming360@gmail.com>
2022-12-29 16:36:22 +0700

    modified:   plugins/nsfw-nhentai.js

--- plugins/nsfw-nhentai.js
+++ plugins/nsfw-nhentai.js
@@ -1,21 +1,54 @@
-import fetch from 'node-fetch'
-import { extractImageThumb } from '@adiwajshing/baileys'
+//Made By Johannes
+//Source https://github.com/FahriAdison/Sad-Multi-Device
 
-let handler = async (m, { conn, args }) => {
-	let code = (args[0] || '').replace(/\D/g, '')
-	if (!code) throw 'Input code' 
+import axios from "axios"
+import PDFDocument from "pdfkit"
+import { extractImageThumb } from "@adiwajshing/baileys"
+import fetch from "node-fetch"
+let handler = async(m, { conn, args }) => {
+
+let code = (args[0] || '').replace(/\D/g, '')
+if (!code) throw 'Input code' 
 	await m.reply('_In progress, please wait..._')
-	let res = await fetch('https://expressjs-akkun.up.railway.app/nhentai?code=' + code)
-	if (!res.ok) throw await res.statusText
-	let json = await res.json()
-	let buffer = await (await fetch('https://external-content.duckduckgo.com/iu/?u=' + json.result.pages[0])).buffer()
-	let jpegThumbnail = await extractImageThumb(buffer)
-	conn.sendMessage(m.chat, { document: { url: 'https://expressjs-akkun.up.railway.app/nhentai/' + code }, jpegThumbnail, fileName: json.result.title + '.pdf', mimetype: 'application/pdf' }, { quoted: m })
-}
-handler.help = ['nhentai']
+let data = await nhentaiScraper(code)
+let pages = []
+let thumb = `https://external-content.duckduckgo.com/iu/?u=https://t.nhentai.net/galleries/${data.media_id}/thumb.jpg`	
+data.images.pages.map((v, i) => {
+			let ext = new URL(v.t).pathname.split('.')[1]
+			pages.push(`https://external-content.duckduckgo.com/iu/?u=https://i7.nhentai.net/galleries/${data.media_id}/${i + 1}.${ext}`)
+		})
+let buffer = await (await fetch(thumb)).buffer()		
+let jpegThumbnail = await extractImageThumb(buffer)		
+let imagepdf = await toPDF(pages)		
+await conn.sendMessage(m.chat, { document: imagepdf, jpegThumbnail, fileName: data.title.english + '.pdf', mimetype: 'application/pdf' }, { quoted: m })
+} 
+handler.command = /^(nhentai|nhpdf)$/i
 handler.tags = ['nsfw']
-handler.command = /^(nhentai)$/i
+handler.help = ['nhentai <code> ']
 handler.premium = true
 handler.limit = true
 
-export default handler
+export default handler 
+
+async function nhentaiScraper(id) {
+	let uri = id ? `https://cin.guru/v/${+id}/` : 'https://cin.guru/'
+	let html = (await axios.get(uri)).data
+	return JSON.parse(html.split('<script id="__NEXT_DATA__" type="application/json">')[1].split('</script>')[0]).props.pageProps.data
+}
+
+function toPDF(images, opt = {}) {
+	return new Promise(async (resolve, reject) => {
+		if (!Array.isArray(images)) images = [images]
+		let buffs = [], doc = new PDFDocument({ margin: 0, size: 'A4' })
+		for (let x = 0; x < images.length; x++) {
+			if (/.webp|.gif/.test(images[x])) continue
+			let data = (await axios.get(images[x], { responseType: 'arraybuffer', ...opt })).data
+			doc.image(data, 0, 0, { fit: [595.28, 841.89], align: 'center', valign: 'center' })
+			if (images.length != x + 1) doc.addPage()
+		}
+		doc.on('data', (chunk) => buffs.push(chunk))
+		doc.on('end', () => resolve(Buffer.concat(buffs)))
+		doc.on('error', (err) => reject(err))
+		doc.end()
+	})
+}

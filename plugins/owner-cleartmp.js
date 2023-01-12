4ae6ae81dd12c03e9a78201cd09e77efc0473cb1
Shirokami Ryzen <infinixgaming360@gmail.com>
2023-01-12 19:48:23 +0700

    modified:   plugins/owner-cleartmp.js 	deleted:    plugins/owner-cleartmp2.js

--- plugins/owner-cleartmp.js
+++ plugins/owner-cleartmp.js
@@ -1,3 +1,4 @@
+/*
 import { tmpdir } from 'os'
 import path, { join } from 'path'
 import {
@@ -23,6 +24,26 @@ const tmp = [tmpdir(), join(__dirname, '../tmp')]
 handler.help = ['cleartmp']
 handler.tags = ['owner']
 handler.command = /^(cleartmp)$/i
+
+handler.rowner = true
+
+export default handler
+*/
+import cp from 'child_process'
+import { promisify } from 'util'
+let exec = promisify(cp.exec).bind(cp)
+let handler = async (m) => {
+	await conn.reply(m.chat, "Done", m)
+    let o
+    try {
+        o = await exec('rm -rf tmp && mkdir tmp')
+    } catch (e) {
+        o = e
+    } 
+}
+handler.help = ['cleartmp2']
+handler.tags = ['owner']
+handler.command = /^(cleartmp2)$/i
 
 handler.rowner = true
 

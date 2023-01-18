2198a35cfd779a148a0cf66a7dec65aeceeeed18
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-01-18 20:36:19 +0700

    modified:   plugins/_openai.js 	modified:   plugins/_openai.js

--- plugins/_openai.js
+++ plugins/_openai.js
@@ -1,6 +1,4 @@
-/**apikey ini punya orang jadi nanti
- kalo abis kalian beli aja sendiri 
-atau cari**/
+/*
 import fetch from 'node-fetch'
 let handler = async (m, {
  text, 
@@ -23,3 +21,27 @@ handler.limit = false
 handler.register = true
 
 export default handler
+*/
+import { Configuration, OpenAIApi } from "openai";
+let handler = async (m, { conn, text }) => {
+if (!text) throw "Contoh:\n${usedPrefix + command} Apa itu OpenAI"
+const configuration = new Configuration({
+    apiKey: ""
+});
+const openai = new OpenAIApi(configuration);
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: text,
+            temperature: 0,
+            max_tokens: 3000,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0
+        });
+            m.reply(response.data.choices[0].text)
+    }
+handler.help = ['ai <pertanyaan>']
+handler.tags = ['main']
+handler.command = /^(ai)$/i
+handler.limit = false
+handler.register = true

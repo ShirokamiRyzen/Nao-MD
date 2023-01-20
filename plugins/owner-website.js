457b784c922c4c9cd8e21a67dc4b77d479e86ad9
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-01-20 17:53:12 +0700

    modified:   plugins/owner-website.js 	deleted:    plugins/revanced.js 	deleted:    plugins/spotify-mod.js

--- plugins/owner-website.js
+++ plugins/owner-website.js
@@ -3,8 +3,15 @@ let handler = async (m, { conn }) => {
   let caption = `
 *「 Owner Website 」*
 
-main page  -> www.ryzn.my.id
-blogspot   -> www.win-tweak.my.id
+main page   -> ryzn.my.id
+blogspot    -> www.win-tweak.my.id
+uptime      -> uptime.ryzn.my.id
+
+----------------------
+
+Revanced    -> ryzn.my.id/revanced
+Spotify Mod -> ryzn.my.id/spotify
+Mod Manager -> ryzn.my.id/mods
 `.trim()
   m.reply(caption)
 }

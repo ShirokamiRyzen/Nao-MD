f2a548a6278a012b732f663e4ebfc2419502853c
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-01-21 13:11:24 +0700

    modified:   plugins/anime-fanart.js

--- plugins/anime-fanart.js
+++ plugins/anime-fanart.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, command }) => {
-	let url = 'https://api.lolhuman.xyz/api/random/art?apikey=8e66d0934cf741bfd2182c16'
+	let url = `https://api.lolhuman.xyz/api/random/art?apikey=${global.lolkey}`
 	conn.sendButton(m.chat, 'Nih FanArtnya ', wm, await(await fetch(url)).buffer(), [['Next',`.${command}`]],m)
 }
 handler.command = /^(fanart)$/i

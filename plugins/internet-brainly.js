8cb29d5742c94a895cbfb102d913ddabcfd58646
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-01-21 15:02:08 +0700

    modified:   plugins/internet-brainly.js

--- plugins/internet-brainly.js
+++ plugins/internet-brainly.js
@@ -1,4 +1,4 @@
-import * as baileys from '@adiwajshing/baileys'
+/* import * as baileys from '@adiwajshing/baileys'
 import { Brainly } from 'brainly-scraper-v2'
 import fetch from 'node-fetch'
 let brainly = new Brainly('id')
@@ -21,6 +21,28 @@ ${v.content}${v.attachments.length > 0 ? `\n*Media Url*: ${v.attachments.join(',
 			await m.reply(`*${answer[x].pertanyaan}*\n_${answer[x].source}_\n${answer[x].jawaban}`)
 			await baileys.delay(2000)
 		}
+	}
+}
+*/
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+	if (!text) throw `Example: ${usedPrefix + command} Soekarno adalah`
+	try {
+		let res = await fetch(`https://api.lolhuman.xyz/api/brainly?apikey=${global.lolkey}&query=${encodeURIComponent(text)}`)
+		let json = await res.json()
+		if (json.status != '200') throw `Informasi tidak tersedia.`
+		let get_result = json.result
+		let ini_txt = "*Result :*"
+		for (var x of get_result) {
+			ini_txt += `\n\n*Question :*\n${x.question.content}\n`
+			ini_txt += `*Answer :*\n${x.answer[0].content}\n`
+			ini_txt += `───────────────────`
+		}
+		await m.reply(ini_txt)
+	} catch (e) {
+		console.log(e)
+		m.reply(`Informasi tidak tersedia.`)
 	}
 }
 handler.help = handler.alias = ['brainly']

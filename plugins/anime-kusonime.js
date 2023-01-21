514260695d22f69bb65cf7fa9ac7cbcc5c6e76a2
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-01-21 15:34:42 +0700

    modified:   plugins/anime-kusonime.js 	deleted:    plugins/anime-otakudesu.js

--- plugins/anime-kusonime.js
+++ plugins/anime-kusonime.js
@@ -1,12 +1,23 @@
 import fetch from 'node-fetch'
-let handler = async function (m, { text, isPrems, isOwner }) {
-	let user = global.db.data.users[m.sender]
-    if (!text) throw '_Ingin Cari Apa_'
-    await m.reply(global.wait)
-  let res = await fetch('https://ardhixsquerpants.herokuapp.com/api/kuso?q=' + encodeURIComponent(text))
-let json= await res.json()
-  const raku =  `*JUDUL :* "${json.title}"\n\n*info:* ${json.info}\n\n*SINOPSIS :* ${json.sinopsis}\n\n*LINK DOWNLOADS:* ${json.link_dl}`
-     conn.sendFile(m.chat,json.thumb, 'image.jpg', raku, m)
+let handler = async (m, { conn, args, text, usedPrefix, command }) => {
+    if (!text) throw `Example : ${usedPrefix + command} Charlotte`
+    try {
+        let res = await fetch(`https://api.lolhuman.xyz/api/kusonime?apikey=${global.lolkey}&url=https://kusonime.com/${encodeURIComponent(text)}-bd-batch-subtitle-indonesia`)
+        let json = await res.json()
+        let get_result = json.result
+        let ini_txt = `*Judul :* "${get_result.japanese}"\n
+*Genre:* ${get_result.genre}\n
+*Season:* ${get_result.seasons}\n
+*Status:* ${get_result.status}\n
+*Total Eps:* ${get_result.total_episode}\n
+*Sinopsis :* ${get_result.desc}\n
+*LINK DOWNLOADS:* ${get_result.link_dl}`
+        
+        conn.sendFile(m.chat, get_result.thumb, 'anime.jpg', ini_txt, m)
+    } catch (e) {
+        console.log(e)
+        m.reply(`Tidak ditemukan hasil.`)
+    }
 }
 handler.help = ['kusonime <judul>']
 handler.tags = ['anime']

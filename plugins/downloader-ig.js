4a977197f81078ada7159d289bcd12dabd5e26e8
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-01-27 17:48:28 +0700

    modified:   plugins/downloader-ig.js

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -1,9 +1,21 @@
+/*
 import instagramGetUrl from 'instagram-url-direct'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.instagram.com/p/ByxKbUSnubS/?utm_source=ig_web_copy_link`
     const results = (await instagramGetUrl(args[0])).url_list[0]
 
     conn.sendFile(m.chat, results, 'instagram.mp4', `*INSTAGRAM DOWNLOADER*`, m)
+}
+*/
+import fg from 'api-dylux'
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `✳️ Use Example\n *${usedPrefix + command}* https://www.instagram.com/p/CYHeKxyMj-J/?igshid=YmMyMTA2M2Y=`
+    m.reply('*Please wait..*.')
+    let res = await fg.igdl(args[0])
+    for (let result of res.url_list) {
+    conn.sendFile(m.chat, result, 'igdl.mp4', `✅ Result`, m)
+  }
 }
 handler.help = ['ig'].map(v => v + ' <url>')
 handler.tags = ['downloader']

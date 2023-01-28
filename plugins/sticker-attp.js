1304e8ed218eae106c05f5fa1d2f40a895e596ae
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-01-28 14:07:05 +0700

    modified:   plugins/sticker-attp.js

--- plugins/sticker-attp.js
+++ plugins/sticker-attp.js
@@ -11,7 +11,7 @@ handler.command = /^attp$/i
 export default handler
 */
 let handler = async(m, { conn, text, args, usedPrefix, command }) => {
-    if (!text) throw `*CONTOH*\n${usedPrefix + command} Bot`
+    if (!text) throw `*CONTOH*\n${usedPrefix + command} Nao Tomori`
     let teks = encodeURI(text)
     if (command == 'attp') {
     conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/attp?apikey=${global.lolkey}&text=${teks}`, 'sticker.webp', '', m, { asSticker: true })}

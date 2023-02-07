b40b80405eee8c905c63b99aa85d771ff309531e
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-02-07 16:53:48 +0700

    modified:   plugins/downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,26 +1,52 @@
-import nodeF from 'node-fetch'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) throw `Masukan URL!\n\ncontoh:\n${usedPrefix + command} https://vm.tiktok.com/ZGJAmhSrp/`
-if (!args[0].match(/tiktok/gi)) throw `URL Tidak Ditemukan!`
- let tioxd = await nodeF(`https://api.botcahx.biz.id/api/dowloader/tikok?url=${args[0]}&apikey=Admin`)
-if (!tioxd.ok) throw await tioxd.text()
+import fetch from 'node-fetch'
+import axios from 'axios'
+import cheerio from 'cheerio'
 
-let tiodl = await tioxd.json()
-if (!tiodl.status) throw tiodl
-let { 
-video, 
-//video2, 
-username,
-description 
-} = tiodl.result
-await conn.sendFile(m.chat, video, 'tiovid.mp4', `
-*Deskripsi*: ${description}
-\n*Username*: ${username}`, m, false, { contextInfo: { forwardingScore: 999, isForwarded: true }})
-
-//conn.sendFile(m.chat, video2, 'tok2.mp4', 'hasil 2', m)
+var handler = async (m, { conn, args}) => {
+  if (!args[0]) throw 'Uhm...url nya mana?'
+    const { thumbnail, video, audio } = await tiktokdl(args[0])
+    const url = video
+    if (!url) throw 'Can\'t download video!'
+    await conn.sendMessage(m.chat, { video: { url: url }},m) 
 }
+
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 handler.command = /^(tiktok|tt|ttdl|tiktokdl)$/i
 
 export default handler
+
+async function tiktokdl (url) {
+    if (!/tiktok/.test(url)) return error.link;
+    const gettoken = await axios.get("https://tikdown.org/id");
+    const $ = cheerio.load(gettoken.data);
+    const token = $("#download-form > input[type=hidden]:nth-child(2)").attr(
+        "value"
+    );
+    const param = {
+        url: url,
+        _token: token,
+    };
+    const {
+        data
+    } = await axios.request("https://tikdown.org/getAjax?", {
+        method: "post",
+        data: new URLSearchParams(Object.entries(param)),
+        headers: {
+            "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "user-agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36",
+        },
+    });
+    var getdata = cheerio.load(data.html);
+    if (data.status) {
+        return {
+            status: true,
+            thumbnail: getdata("img").attr("src"),
+            video: getdata("div.download-links > div:nth-child(1) > a").attr("href"),
+            audio: getdata("div.download-links > div:nth-child(2) > a").attr("href"),
+        };
+    } else
+        return {
+            status: false,
+        };
+};

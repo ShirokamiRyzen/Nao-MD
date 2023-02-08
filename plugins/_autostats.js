725fd6512a239437cb23034378aea96f4a8ad3eb
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-02-08 16:03:14 +0700

    modified:   plugins/_autostats.js

--- plugins/_autostats.js
+++ plugins/_autostats.js
@@ -1,11 +1,18 @@
+import os from 'os'
 export async function all(m) {
 	let setting = db.data.settings[this.user.jid]
 
 	if (new Date() * 1 - setting.status > 1000) {
 		let _uptime = process.uptime() * 1000
 		let uptime = clockString(_uptime);
-		let wm = 'Panel'
-		let bio = `🗓️Aktif selama ${uptime} | Mode: ${global.opts['self'] ? 'Private' : setting.self ? 'Private' : global.opts['gconly'] ? 'Hanya Grup' : 'Publik'} |  AmeliaBotz ${wm}`
+		
+		let plat = os.platform()
+let emot1 = `${pickRandom(['⎔', '◈▻', '✦', '⭑', 'ᯬ', '⭔', '◉', '⬟', '᭻', '»', '〆', '々', '⛥', '✗', '⛊', '⚜', '⚝', '⚚', '♪'])}`
+let emot2 = `${pickRandom(['⎔', '◈▻', '✦', '⭑', 'ᯬ', '⭔', '◉', '⬟', '᭻', '»', '〆', '々', '⛥', '✗', '⛊', '⚜', '⚝', '⚚', '♪'])}`
+let emot3 = `${pickRandom(['⎔', '◈▻', '✦', '⭑', 'ᯬ', '⭔', '◉', '⬟', '᭻', '»', '〆', '々', '⛥', '✗', '⛊', '⚜', '⚝', '⚚', '♪'])}`
+let emot4 = `${pickRandom(['⎔', '◈▻', '✦', '⭑', 'ᯬ', '⭔', '◉', '⬟', '᭻', '»', '〆', '々', '⛥', '✗', '⛊', '⚜', '⚝', '⚚', '♪'])}`
+
+let bio = `${emot1} Halo ${await conn.getName(m.sender)} ${emot2} Runtime ${uptime} ${emot3} Mode: ${global.opts['self'] ? 'Private' : setting.self ? 'Private' : global.opts['gconly'] ? 'Hanya Grup' : 'Publik'} ${emot4} Platform: ${plat} | ${wm} `
 
 		await this.setBio(bio).catch(_ => _)
 		setting.status = new Date() * 1
@@ -19,5 +26,6 @@ function clockString(ms) {
 	return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
 }
 
-
-// buatan FokusDotId (Fokus ID)
+function pickRandom(list) {
+return list[Math.floor(Math.random() * list.length)]
+}

2b3f0d155838297d6978e3b8dcf1822ff97de3ba
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-02-09 17:23:09 +0700

    new file:   plugins/_firstchat.js

---
+++ plugins/_firstchat.js
@@ -0,0 +1,35 @@
+import moment from 'moment-timezone';
+
+export async function before(m) {
+    if (m.chat.endsWith('broadcast') || m.fromMe || m.isGroup) return
+
+    let user = global.db.data.users[m.sender]
+    let txt = `👋Hai, ${ucapan()}
+${user.banned ? '📮Maaf, kamu dibanned & Tidak bisa menggunakan bot ini lagi' : `💬 Ada yg bisa ${this.user.name} bantu?`}`.trim()
+
+    if (new Date() - user.pc < 21600000) return // waktu ori 21600000 (6 jam)
+    await this.sendButton(m.chat, txt, user.banned ? wm : '📮Note: Jangan spam bot nya', [user.banned ? 'OWNER' : 'OWNER', user.banned ? '.owner' : '.owner'], m)
+    user.pc = new Date * 1
+}
+
+
+function ucapan() {
+    const time = moment.tz('Asia/Jakarta').format('HH')
+    let res = "Selamat dinihari 🌆"
+    if (time >= 4) {
+        res = "Selamat pagi 🌄"
+    }
+    if (time > 10) {
+        res = "Selamat siang ☀️"
+    }
+    if (time >= 15) {
+        res = "Selamat sore 🌇"
+    }
+    if (time >= 18) {
+        res = "Selamat malam 🌙"
+    }
+    return res
+}
+
+
+// jasa buat plugins by FokusDotId (Fokus ID)

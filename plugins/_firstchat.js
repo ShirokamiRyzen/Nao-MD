d193e9642d6b8fbd8943ba57c34b29693d6d4298
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-02-11 09:56:05 +0700

    modified:   plugins/_firstchat.js

--- plugins/_firstchat.js
+++ plugins/_firstchat.js
@@ -8,7 +8,7 @@ export async function before(m) {
 ${user.banned ? '📮Maaf, kamu dibanned & Tidak bisa menggunakan bot ini lagi' : `💬 Ada yg bisa ${this.user.name} bantu?`}`.trim()
 
     if (new Date() - user.pc < 21600000) return // waktu ori 21600000 (6 jam)
-    await this.sendButton(m.chat, txt, user.banned ? wm : '📮Note: Jangan spam bot nya', [user.banned ? 'OWNER' : 'OWNER', user.banned ? '.owner' : '.owner'], m)
+    await this.sendButton(m.chat, txt, user.banned ? wm : '📮Note: Jangan spam botnya', [user.banned ? 'MENU' : 'MENU', user.banned ? '.menu' : '.menu'], m)
     user.pc = new Date * 1
 }
 

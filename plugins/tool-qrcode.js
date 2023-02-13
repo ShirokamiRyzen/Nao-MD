3ffc99e768cb74f1f8a9d4fd1830b2d665c658cf
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-02-13 17:39:32 +0700

    modified:   plugins/tool-qrcode.js

--- plugins/tool-qrcode.js
+++ plugins/tool-qrcode.js
@@ -1,7 +1,10 @@
-
 import { toDataURL } from 'qrcode'
 
-let handler = async (m, { conn, text }) => conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', '¯\\_(ツ)_/¯', m)
+let handler = async (m, { conn, text }) => {
+    if (!text) return conn.reply(m.chat, 'Use example: \n.qrcode Shirokami Ryzen', m)
+
+conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', '¯\\_(ツ)_/¯', m)
+}
 
 handler.help = ['', 'code'].map(v => 'qr' + v + ' <teks>')
 handler.tags = ['tools']

e68e23d359e69a58d5d53baf3f60c17e5553e211
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-02-16 15:43:49 +0700

    new file:   plugins/downloader-danbooru.js 	modified:   plugins/tool-chord.js

---
+++ plugins/downloader-danbooru.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+import cheerio from 'cheerio'
+
+let handler = async (m, { conn, args }) => {
+	if (!args[0]) throw 'Input URL'
+	if (!/danbooru\.donmai\.us\/posts\/[0-9]+$/i.test(args[0])) throw 'Invalid URL'
+	await m.reply(wait)
+	let data = await danbooruDl(args[0]), img = data.url
+	delete data.url
+	let capt = Object.keys(data).map((x) => `${x}: ${data[x]}`).join`\n`
+	await conn.sendFile(m.chat, img, '', capt, m)
+}
+handler.tags = ['downloader']
+handler.command = /^danbooru$/i
+handler.help = ['Danbooru']
+
+export default handler
+
+export async function danbooruDl(url) {
+	let html = (await axios.get(url)).data
+	let $ = cheerio.load(html), obj = {}
+	$('#post-information > ul > li').each((idx, el) => {
+		let str = $(el).text().trim().replace(/\n/g, '').split(': ')
+		obj[str[0]] = str[1].replace('»', '').trim().split(' .')[0]
+	})
+	obj.url = $('#post-information > ul > li[id="post-info-size"] > a').attr('href')
+	return obj
+}
--- plugins/tool-chord.js
+++ plugins/tool-chord.js
@@ -1,18 +1,33 @@
 import axios from 'axios'
+import cheerio from 'cheerio'
 
-let handler = async(m, { conn, text, usedPrefix }) => {
-
-    if (!text) return conn.reply(m.chat, 'Contoh penggunaan: ' + usedPrefix + 'chord hanya rindu', m)
-    axios.get(`https://docs-jojo.herokuapp.com/api/chord?q=` + text)
-        .then((res) => {
-          let hasil = `*• Chord Lagu ${text} :*\n${res.data.result}`
-            conn.reply(m.chat, hasil, m)
-        })
-        .catch(_ => m.reply('Chord Lagu Tidak Ditemukan!'))
+let handler = async (m, { text }) => {
+	if (!text) throw 'Input Query'
+	let a = await chord(text)
+	m.reply(`*Song :* ${a.title}\n*Chord :*\n\n${a.chord}`)
 }
+
 handler.help = ['chord <judul lagu>']
 handler.tags = ['tools']
 handler.command = /^(chord)$/i
+
 handler.limit = true
 
-export default handler 
+export default handler
+
+export async function chord(query) {
+  return new Promise(async(resolve, reject) => {
+   const head = {"User-Agent":"Mozilla/5.0 (Linux; Android 9; CPH1923) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.62 Mobile Safari/537.36",
+	  "Cookie":"__gads=ID=4513c7600f23e1b2-22b06ccbebcc00d1:T=1635371139:RT=1635371139:S=ALNI_MYShBeii6AFkeysWDKiD3RyJ1106Q; _ga=GA1.2.409783375.1635371138; _gid=GA1.2.1157186793.1635371140; _fbp=fb.1.1635371147163.1785445876"};
+  let { data } = await axios.get("http://app.chordindonesia.com/?json=get_search_results&exclude=date,modified,attachments,comment_count,comment_status,thumbnail,thumbnail_images,author,excerpt,content,categories,tags,comments,custom_fields&search="+query, {headers: head});
+	axios.get("http://app.chordindonesia.com/?json=get_post&id="+data.posts[0].id, {
+	  headers: head
+	}).then(anu => {
+	  let $ = cheerio.load(anu.data.post.content);
+	  resolve({
+	    title: $("img").attr("alt"),
+	    chord: $("pre").text().trim()
+	  });
+	}).catch(reject);
+});
+}

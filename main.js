5df8db7763790c4f0ccb50911ae0ab44af54d6d3
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-02-16 21:37:33 +0700

    modified:   main.js

--- main.js
+++ main.js
@@ -83,7 +83,7 @@ global.loadDatabase = async function loadDatabase() {
   global.db.chain = chain(global.db.data)
 }
 loadDatabase()
-global.authFile = `${opts._[0] || 'session'}.data.json`
+global.authFile = `${opts._[0] || 'session'}.ryzen.json`
 const { state, saveState } = useSingleFileAuthState(global.authFile)
 
 const connectionOptions = {

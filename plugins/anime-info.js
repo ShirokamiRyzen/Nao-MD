10e41cb857f4c4ef8d80521b49c6fc560e511191
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-02-21 18:07:56 +0700

    modified:   plugins/anime-info.js

--- plugins/anime-info.js
+++ plugins/anime-info.js
@@ -1,39 +1,25 @@
 import fetch from 'node-fetch'
-let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-    if (!text) throw `Example : ${usedPrefix + command} Charlotte`
-    try {
-        let res = await fetch(`https://api.lolhuman.xyz/api/anime?apikey=${global.lolkey}&query=${text}`)
-        let json = await res.json()
-        let get_result = json.result
-        let ini_txt = `Title : *${get_result.title.romaji}*\n`
-        ini_txt += `English : *${get_result.title.english}*\n`
-        ini_txt += `Native : *${get_result.title.native}*\n\n`
-        ini_txt += `Format : ${get_result.format}\n`
-        ini_txt += `Episodes : ${get_result.episodes}\n`
-        ini_txt += `Duration : ${get_result.duration} mins.\n`
-        ini_txt += `Status : ${get_result.status}\n`
-        ini_txt += `Season : ${get_result.season}\n`
-        ini_txt += `Season Year : ${get_result.seasonYear}\n`
-        ini_txt += `Source : ${get_result.source}\n`
-        ini_txt += `Start Date : ${get_result.startDate.day} - ${get_result.startDate.month} - ${get_result.startDate.year}\n`
-        ini_txt += `End Date : ${get_result.endDate.day} - ${get_result.endDate.month} - ${get_result.endDate.year}\n`
-        ini_txt += `Genre : ${get_result.genres.join(", ")}\n`
-        ini_txt += `Synonyms : ${get_result.synonyms.join(", ")}\n`
-        ini_txt += `*Score : ${get_result.averageScore}%*\n\n`
-        ini_txt += `*Characters :*\n`
-        let ini_character = get_result.characters.nodes
-        for (var x of ini_character) {
-            ini_txt += `- ${x.name.full} (${x.name.native})\n`
-        }
-        ini_txt += `\n*Description :*\n${get_result.description}`
-        conn.sendFile(m.chat, get_result.coverImage.large, 'anianime.jpg', ini_txt, m)
-    } catch (e) {
-        console.log(e)
-        m.reply(`Tidak ditemukan hasil.`)
-    }
+let handler = async(m, { conn, text }) => {
+  if (!text) throw `Masukkan query!`
+  let res = await fetch(global.API('https://api.jikan.moe', '/v4/anime', { q: text }))
+  if (!res.ok) throw await res.text()
+  let json = await res.json()
+  let { title, members, synopsis, episodes, url, rated, score, image_url, type, start_date, end_date } = json.data[0]
+let animeingfo = `✨️ *Title:* ${title}
+🎆️ *Episodes:* ${episodes}
+➡️ *Start date:* ${start_date}
+🔚 *End date:* ${end_date}
+💬 *Show Type:* ${type}
+💌️ *Rating:* ${rated}
+❤️ *Score:* ${score}
+👥 *Members:* ${members}
+💚️ *Synopsis:* ${synopsis}
+🌐️ *URL*: ${url}`
+  conn.sendFile(m.chat, image_url, '', animeingfo, m)
 }
+
 handler.help = ['animeinfo <anime>']
 handler.tags = ['anime', 'limitmenu']
 handler.command = /^(animeinfo)$/i
-handler.limit = true
+handler.limit = false
 export default handler

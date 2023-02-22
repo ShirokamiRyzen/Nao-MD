c26070d3e4997b8fbef5c8372b82026a3512498f
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-02-22 21:27:39 +0700

    modified:   plugins/_antitoxic.js

--- plugins/_antitoxic.js
+++ plugins/_antitoxic.js
@@ -1,3 +1,4 @@
+/*
 let badwordRegex = /anj(k|g)|ajn?(g|k)|a?njin(g|k)|bajingan|b(a?n)?gsa?t|ko?nto?l|me?me?(k|q)|pe?pe?(k|q)|meki|titi(t|d)|pe?ler|tetek|toket|ngewe|go?blo?k|to?lo?l|idiot|(k|ng)e?nto?(t|d)|jembut|bego|dajj?al|janc(u|o)k|pantek|puki ?(mak)?|kimak|kampang|lonte|col(i|mek?)|pelacur|henceu?t|nigga|fuck|dick|bitch|tits|bastard|asshole/i // tambahin sendiri
 
 export function before(m, { isBotAdmin }) {
@@ -29,3 +30,28 @@ Anda hanya bisa meminta Owner untuk membuka banned-nya !\n\n🌸 Nao-Botz
     }
     return !0
 }
+*/
+
+let handler = m => m
+
+let toxic = /(a(su|nj(([ie])ng|([ie])r)?)|me?me?k|ko?nto?l|ba?bi|fu?ck|ta(e|i)k|bangsat|g([iueo])bl([iueo])(k|g)|g ([iueo]) b l ([iueo]) (k|g)|a (n j (i n g|i r)?)s u|col(i|ay)|an?jg|b([ia])ngs([ia])?t|t([iuo])l([iuo])l)/i
+handler.before = function (m, { user }) {
+  if (m.isBaileys && m.fromMe) return true
+  if (/masuk|lanjutkan|banjir|(per)?panjang/g.exec(m.text)) return true
+  let chat = global.db.data.chats[m.chat]
+  let isGroupToxic = toxic.exec(m.text)
+
+  if (chat.antiToxic && isGroupToxic) {
+    m.reply('Jangan Toxic ya!!\n' + readMore + '\nMau Matikan? ketik */disable antitoxic*')
+    if (global.opts['restrict']) {
+      // if (!user.isAdmin) return true
+      // this.groupRemove(m.chat, [m.sender])
+    }
+  }
+  return true
+}
+
+export default handler
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)

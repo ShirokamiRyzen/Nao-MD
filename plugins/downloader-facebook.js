f2c04e34651a974f771d529fcbef5e513c01859b
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-03-05 20:28:07 +0700

    modified:   plugins/downloader-facebook.js

--- plugins/downloader-facebook.js
+++ plugins/downloader-facebook.js
@@ -1,24 +1,67 @@
-import fg from 'api-dylux'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
- 
- if (!args[0]) throw `✳️ Masukan link video Facebook\n\n📌 Contoh:\n*${usedPrefix + command}* https://fb.watch/d7nB8-L-gR/`
-    
-   try {
-   let res = await fg.fbdl(args[0])
-    for (let result of res.download) {
-    	  let tex = `
-┌─⊷ *FBDL*
-▢ *Resolution:* ${result.quality}
-└───────────`
-    conn.sendFile(m.chat, result.url, 'fb.mp4', tex, m)
-     } 
-   
- } catch {
- 	m.reply('Error: Link not found')
- 	} 
+import axios from 'axios'
+var handler = async (m, { conn, command, text, usedPrefix }) => {
+	if (!text) throw `Use example\n${usedPrefix}${command} https://www.facebook.com/100010929794713/posts/1885825845125057/`
+	const req = await igeh(text)
+	const { url_list } = req
+    return conn.sendMessage(m.chat, { video : { url : url_list }}, m) 
 }
-handler.help = ['facebook'].map(v => v + ' <url>')
+
+handler.help = ['fb'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^((facebook|fb)(downloder|dl)?)$/i
+
+handler.command = /^(fb(dl)?)$/i
 
 export default handler
+
+async function igeh(url_media){
+    return new Promise(async (resolve,reject)=>{
+        const BASE_URL = "https://instasupersave.com/"
+        
+        //New Session = Cookies
+        try {
+            const resp = await axios(BASE_URL);
+            const cookie = resp.headers["set-cookie"]; // get cookie from request
+            const session = cookie[0].split(";")[0].replace("XSRF-TOKEN=","").replace("%3D", "")
+            
+            //REQUEST CONFIG
+            var config = {
+                method: 'post',
+                url: `${BASE_URL}api/convert`,
+                headers: { 
+                    'origin': 'https://instasupersave.com', 
+                    'referer': 'https://instasupersave.com/pt/', 
+                    'sec-fetch-dest': 'empty', 
+                    'sec-fetch-mode': 'cors', 
+                    'sec-fetch-site': 'same-origin', 
+                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.52', 
+                    'x-xsrf-token': session, 
+                    'Content-Type': 'application/json', 
+                    'Cookie': `XSRF-TOKEN=${session}; instasupersave_session=${session}`
+                },
+                data : {
+                    url: url_media
+                }
+            };
+
+            //REQUEST
+            axios(config).then(function (response) {
+                let ig = []
+                if(Array.isArray(response.data)){
+                    response.data.forEach(post => { ig.push(post.sd === undefined ? post.thumb : post.sd.url)})
+                } else {
+                    ig.push(response.data.url[0].url)    
+                }
+                
+                resolve({
+                    results_number : ig.length,
+                    url_list: ig
+                })
+            })
+            .catch(function (error) {
+                reject(error.message)
+            })
+        } catch(e){
+            reject(e.message)
+        }
+    })
+}

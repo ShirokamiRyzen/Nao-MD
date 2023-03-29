b6ed09383342137995695d8594b76206816b0800
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-03-29 16:58:30 +0700

    modified:   plugins/afk-_afk.js 	modified:   plugins/afk-afk.js

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -1,24 +1,11 @@
-import fs from 'fs'
-import fetch from 'node-fetch'
-let handler = async (m, { text }) => {
-let name = m.pushName || conn.getName(m.sender)
-
-let user = global.db.data.users[m.sender]
-let wibu = `${global.hwaifu}`
-
-let thumb = await(await fetch(wibu)).buffer()
-user.afk = + new Date
-user.afkReason = text
- conn.sendButtonDoc(m.chat, `${conn.getName(m.sender)} Sedang AFK Dengan Alasan ⬋`, `⬕ ${text ? ': ' + text : ''}` , 'Jangan Ganggu Ya', 'huuuuu', m,  { contextInfo: { externalAdReply: { showAdAttribution: true,
-        title: 'Simple Bot Ryzn',
-        body: wm,
-        thumbnail: thumb,
-        sourceUrl: sgc
-    }
-    } })
-            }
-handler.help = ['afk [alasan]']
-handler.tags = ['main']
-handler.command = /^afk$/i
-
-export default handler
+var handler = async (m, { text }) => {
+    let user = global.db.data.users[m.sender]
+    user.afk = + new Date
+    user.afkReason = text
+    m.reply(`${conn.getName(m.sender)} is now AFK${text ? ': ' + text : ''}`)
+  }
+  handler.help = ['afk [alasan]']
+  handler.tags = ['main']
+  handler.command = /^afk$/i
+  
+  export default handler

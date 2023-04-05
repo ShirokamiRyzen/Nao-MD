93621cf50e743db2a100c575b5f180339026510a
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-04-05 15:49:05 +0700

    fix

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -1,17 +1,18 @@
-import moment from 'moment-timezone'
-let handler = async (m, { conn, text, participants, isAdmin, isOwner }) => {
-    let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    let bcbg = 'https://telegra.ph/file/da19d1c4c1e23b8b9c72e.jpg'
-    conn.send3ButtonLoc(m.chat, bcbg, wm, `${text ? `${text}\n` : ''}┌─「 Tag All 」\n` + users.map(v => '│◦❒ @' + v.replace(/@.+/, '')).join`\n` + '\n└────','Menu', '.menu', 'Owner', '.owner', `\nKakek Gw Sugiono`, '.huuu', null,
- /*let m.reply(`${text ? `${text}\n` : ''}┌─「 Tag All 」\n` + users.map(v => '│◦❒ @' + v.replace(/@.+/, '')).join`\n` + '\n└────', null, */{
-        mentions: users
+// Code By Xnuvers007
+// https://github.com/Xnuvers007
+/////////////////////////////////
+
+let handler = async (m, { conn, text, participants }) => {
+    let users = participants.map(u => u.jid).filter(Boolean) // filter element yang tidak valid
+    m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
+      contextInfo: { mentionedJid: users }
     })
-}
-
-handler.help = ['tagall']
-handler.tags = ['group']
-handler.command = ['tagall']
-handler.admin = true
-handler.group = true
-
-export default handler
+  }
+  
+  handler.help = ['tagall']
+  handler.tags = ['group']
+  handler.command = ['tagall']
+  handler.admin = true
+  handler.group = true
+  
+  export default handler
--- plugins/owner-kick.js
+++ plugins/owner-kick.js
@@ -1,24 +1,32 @@
+// Code by Xnuvers007
+// https://github.com/Xnuvers007/
+
 import { areJidsSameUser } from '@adiwajshing/baileys'
-let handler = async (m, { conn, participants }) => {
+
+let handler = async (m, { conn, participants, isAdmin }) => {
+    if (!isAdmin) {
+        return m.reply('Perintah ini hanya dapat digunakan oleh admin grup')
+    }
+
     let users = m.mentionedJid.filter(u => !areJidsSameUser(u, conn.user.id))
     let kickedUser = []
     for (let user of users)
         if (user.endsWith('@s.whatsapp.net') && !(participants.find(v => areJidsSameUser(v.id, user)) || { admin: true }).admin) {
-            const res = await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
+            const res = await conn.groupParticipantsUpdate(m.chat, [user], "remove")
             kickedUser.concat(res)
             await delay(1 * 1000)
         }
-    m.reply(`Succes kick ${kickedUser.map(v => '@' + v.split('@')[0])}`, null, { mentions: kickedUser })
-
+    m.reply(`Mampos Dikick Kau ${kickedUser.map(v => '@' + v.split('@')[0])}`, null, { mentions: kickedUser })
 }
-handler.help = ['kick', '-'].map(v => 'o' + v + ' @user')
-handler.tags = ['owner']
-handler.command = /^(okick|o-)$/i
 
-handler.admin = true
+handler.help = ['kick'].map(v => v + ' @user')
+handler.tags = ['group']
+handler.command = /^(kick)$/i
+
+handler.owner = false
 handler.group = true
 handler.botAdmin = true
-
-export default handler
+handler.admin = true // hanya admin grup yang dapat menggunakan perintah ini
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+export default handler

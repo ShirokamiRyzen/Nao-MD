a35982ff070fd1c3b3acc8f525dd1219354212da
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-04-18 08:31:29 +0700

    fix openai

--- plugins/_openai.js
+++ plugins/_openai.js
@@ -1,29 +1,34 @@
-import fetch from "node-fetch"
-import { generateWAMessageFromContent } from "@adiwajshing/baileys"
-import fs from 'fs'
-import { Configuration, OpenAIApi } from 'openai'
-const configuration = new Configuration({ organization: `${global.org}`, apiKey: `${global.openai}` });
-const openai = new OpenAIApi(configuration);
-let handler = async (m, { conn, text }) => {
-try {
-            if (!text) return m.reply(`Chat dengan AI.\n\nContoh:\n${usedPrefix}${command} Halo? `);
+import fetch from "node-fetch";
+import { generateWAMessageFromContent } from "@adiwajshing/baileys";
+import fs from 'fs';
+import { Configuration, OpenAIApi } from 'openai';
 
-            const response = await openai.createChatCompletion({
-          model: "gpt-3.5-turbo",
-          messages: [{role: "user", content: text}],
-          });
-          m.reply(`${response.data.choices[0].message.content}`);
-          } catch (error) {
-          if (error.response) {
-            console.log(error.response.status);
-            console.log(error.response.data);
-            console.log(`${error.response.status}\n\n${error.response.data}`);
-          } else {
-            console.log(error);
-            m.reply("Maaf, sepertinya ada yang error :"+ error.message);
-          }
-        }
+const configuration = new Configuration({ organization: `${global.org}`, apiKey: `${global.openai}` }); //KEY-OPENAI-APIKEY-KAMU = https://platform.openai.com/account/api-keys , KEY-ORG-KAMU = https://platform.openai.com/account/org-settings
+const openai = new OpenAIApi(configuration);
+
+let handler = async (m, { conn, usedPrefix, command, text }) => {
+  try {
+    if (!text) throw new Error(`Chat dengan AI.\n\nContoh:\n${usedPrefix}${command} Halo?`);
+
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: text }],
+    });
+
+    conn.reply(m.chat, `${response.data.choices[0].message.content}`, m);
+
+  } catch (error) {
+    console.log(error);
+    if (error.response) {
+      console.log(error.response.status);
+      console.log(error.response.data);
+      conn.reply(m.chat, `${error.response.status}\n\n${error.response.data}`, m);
+    } else {
+      conn.reply(m.chat, `${error.message}`, m);
+    }
+  }
 }
+
 handler.help = ['ai <pertanyaan>']
 handler.tags = ['ai']
 handler.command = /^(ai)$/i

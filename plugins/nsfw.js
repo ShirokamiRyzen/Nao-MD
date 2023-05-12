b0e3d6b3265951539cc2c63f111d86ce3dbf84af
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-05-12 20:59:43 +0700

    hehe

--- plugins/nsfw.js
+++ plugins/nsfw.js
@@ -2,11 +2,11 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn, usedPrefix, command, text, args }) => {
   if (!args[0]) throw `Use example .lewd neko`
-	let res = await fetch(`https://api.waifu.pics/nsfw/${text}`)
-  
-	if (!res.ok) throw await res.text()
-	let json = await res.json()
-	conn.sendButton(m.chat, `Nyaww~ 🐾💗 ${command.capitalize()}`, json.url, json.url, [['next', `${usedPrefix}lewd ${text}`]], m)
+  let res = await fetch(`https://api.waifu.pics/nsfw/${text}`)
+  if (!res.ok) throw await res.text()
+  let json = await res.json()
+  if (!json.url) throw 'Error!'
+  conn.sendFile(m.chat, json.url, '', '2023 © Nao Bot V2', m)
 }
 handler.command = /^(lewd)$/i
 handler.premium = true
--- plugins/sfw.js
+++ plugins/sfw.js
@@ -2,11 +2,11 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn, usedPrefix, command, text, args }) => {
   if (!args[0]) throw `Use example .pic neko`
-	let res = await fetch(`https://api.waifu.pics/sfw/${text}`)
-  
-	if (!res.ok) throw await res.text()
-	let json = await res.json()
-	conn.sendButton(m.chat, `Nyaww~ 🐾💗 ${command.capitalize()}`, json.url, json.url, [['next', `${usedPrefix}pic ${text}`]], m)
+  let res = await fetch(`https://api.waifu.pics/sfw/${text}`)
+  if (!res.ok) throw await res.text()
+  let json = await res.json()
+  if (!json.url) throw 'Error!'
+  conn.sendFile(m.chat, json.url, '', '2023 © Nao Bot V2', m)
 }
 
 handler.tags = ['anime']

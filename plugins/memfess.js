c031a0e2256475e7014cc5c5882774e3f5234ebb
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-05-20 15:22:49 +0700

    goodbye button function

--- plugins/memfess.js
+++ plugins/memfess.js
@@ -1,3 +1,7 @@
+import fs from 'fs'
+import fetch from 'node-fetch'
+import moment from 'moment-timezone'
+
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     conn.menfess = conn.menfess ? conn.menfess : {}
     if (!text) throw `*Cara penggunaan :*\n\n${usedPrefix + command} nomor|nama pengirim|pesan\n\n*Note:* nama pengirim boleh nama samaran atau anonymous.\n\n*Contoh:* ${usedPrefix + command} ${m.sender.split`@`[0]}|${namebot}|Halo.`;
@@ -7,14 +11,14 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     let data = (await conn.onWhatsApp(jid))[0] || {};
     if (!data.exists) throw 'Nomer tidak terdaftar di whatsapp.';
     
-   // if (jid == m.sender) throw 'tidak bisa mengirim pesan memfess ke diri sendiri.'
+   if (jid == m.sender) throw 'tidak bisa mengirim pesan menfess ke diri sendiri.'
     
     let mf = Object.values(conn.menfess).find(mf => mf.status === true)
     if (mf) return !0
     try {
     	let id = + new Date
         let txt = `Hai @${data.jid.split('@')[0]}, kamu menerima pesan Menfess nih.\n\nDari: *${name}*\nPesan: \n${pesan}\n\nMau balas pesan ini kak? bisa kak. kakak tinggal ketik pesan kakak nanti saya sampaikan ke *${name}*.`.trim();
-        await conn.sendButton(data.jid, txt, wm, null, [[' ']])
+        await conn.reply(data.jid, txt, m, { contextInfo: { externalAdReply: {title: global.wm, body: 'Nao Bot V2', sourceUrl: global.snh, thumbnail: fs.readFileSync('./thumbnail.jpg') }}})
         .then(() => {
             m.reply('Berhasil mengirim pesan menfess.')
             conn.menfess[id] = {

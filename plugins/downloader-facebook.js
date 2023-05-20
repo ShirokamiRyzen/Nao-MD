f28ce7d4e9250cb91d4440bd06ca114f4edca5cf
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-05-20 16:08:35 +0700

    update API

--- plugins/downloader-facebook.js
+++ plugins/downloader-facebook.js
@@ -4,11 +4,11 @@ let handler = async (m, { conn, usedPrefix, args, command, text }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let name = await conn.getName(who)
 if (!args[0]) throw `Linknya?`
-  let res = await fetch(`http://api.betabotz.me/api/download/fbdown?url=${args[0]}&apikey=${global.lankey}`)
+  let res = await fetch(`http://saipulanuar.ga/api/download/fb?url=${args[0]}`)
   let x = await res.json()
   m.reply('Tunggu Sebentar...')
   let cap = `Nih Kak Videonya >,<`
-  conn.sendFile(m.chat, x.result.Normal_video, 'fb.mp4', cap, m)
+  conn.sendFile(m.chat, x.result.hd, 'fb.mp4', cap, m)
 }
 
 handler.help = ['fb'].map(v => v + ' <url>')

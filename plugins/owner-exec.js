dc323f41ba2341dee93a73ac5ed816391ef952a3
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-05-20 13:48:16 +0700

    p

--- plugins/owner-exec.js
+++ plugins/owner-exec.js
@@ -1,9 +1,8 @@
 import cp, { exec as _exec } from 'child_process'
 import { promisify } from 'util'
-let exec = promisify(_exec).bind(cp)
+let exec = promisify(cp.exec).bind(cp)
 let handler = async (m, { conn, isOwner, command, text }) => {
   if (global.conn.user.jid != conn.user.jid) return
-  m.reply('Executing...')
   let o
   try {
     o = await exec(command.trimStart()  + ' ' + text.trimEnd())

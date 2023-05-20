18301e0120a81243e0c736ff446b7d84fe2effa7
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-05-20 11:05:38 +0700

    fix yt

--- plugins/youtube-yta.js
+++ plugins/youtube-yta.js
@@ -1,80 +1,6 @@
-/*
-import fetch from 'node-fetch'
-import { youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper'
-import { niceBytes, isUrl, somematch } from '../lib/others.js'
-
-let handler = async (m, { conn, text, args, command }) => {
-	if (!text.match(new RegExp(/(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed|shorts)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9\_-]+)/, 'gi'))) return m.reply(`Invalid Youtube URL.`)
-	command = command.toLowerCase()
-	try {
-		const { audio: _audio, title } = await youtubedl(args[0]).catch(async _ => await youtubedlv2(args[0])).catch(async _ => await youtubedlv3(args[0]))
-		let audio, source, res, link, lastError, sizeh
-		for (let i in _audio) {
-			try {
-				audio = _audio[i]
-				link = await audio.download()
-				sizeh = await audio.fileSize
-				if (link) res = await fetch(link)
-				if (res) source = await res.arrayBuffer()
-				if (source instanceof ArrayBuffer) break
-			} catch (e) {
-				audio = link = source = null
-				lastError = e
-			}
-		}
-		if (sizeh > 300000) throw `Filesize: ${audio.fileSizeH}\nTidak dapat mengirim, maksimal file 300 MB`
-		if (!link) throw new Error('No URL')
-		if (command.includes('mp3')) await conn.sendMessage(m.chat, { document: { url: link }, mimetype: 'audio/mpeg', fileName: `${title}.mp3`}, { quoted : m })
-		else await conn.sendMessage(m.chat, { audio: { url: link }, mimetype: 'audio/mp4' }, { quoted : m })
-	} catch (e) {
-		console.log(e)
-		try {
-			let res = await fetch(`https://api.lolhuman.xyz/api/ytaudio?apikey=${global.lolkey}&url=${text}`)
-			let anu = await res.json()
-			anu = anu.result
-			let vsize = anu.link.size.slice(-2)
-			if (vsize == "GB") return m.reply(`Ngotak dong.\nMana bisa ngirim video ${anu.link.size}`)
-			if (!somematch(['kB','KB'], vsize) && parseInt(anu.link.size.replace(" MB", "")) > 200) return m.reply(`Filesize: ${anu.link.size}\nTidak dapat mengirim, maksimal file 300 MB`)
-			if (!anu.link.link) throw new Error('Error')
-			if (command.includes('mp3')) await conn.sendMessage(m.chat, {document: { url: anu.link.link }, mimetype: 'audio/mpeg', fileName: `${anu.result.title}.mp3`}, { quoted : m })
-			else await conn.sendMessage(m.chat, { audio: { url: anu.link.link }, mimetype: 'audio/mp4' }, { quoted : m })
-		} catch (e) {
-			console.log(e)
-			try {
-				let res = await fetch(`https://api.lolhuman.xyz/api/ytaudio2?apikey=${global.lolkey}&url=${text}`)
-				let anu = await res.json()
-				anu = anu.result
-				let vsize = anu.size.slice(-2)
-				if (vsize == "GB") return m.reply(`Ngotak dong.\nMana bisa ngirim video ${anu.size}`)
-				if (!somematch(['kB','KB'], vsize) && parseInt(anu.size.replace(" MB", "")) > 200) return m.reply(`Filesize: ${anu.size}\nTidak dapat mengirim, maksimal file 300 MB`)
-				if (!anu.link) throw new Error('Error')
-				if (command.includes('mp3')) await conn.sendMessage(m.chat, {document: { url: anu.link }, mimetype: 'audio/mpeg', fileName: `${anu.result.title}.mp3`}, { quoted : m })
-				else await conn.sendMessage(m.chat, { audio: { url: anu.link }, mimetype: 'audio/mp4' }, { quoted : m })
-			} catch (e) {
-				console.log(e)
-				throw `Invalid Youtube URL / terjadi kesalahan.`
-			}
-		}
-	}
-}
-
-handler.help = ['mp3', 'a'].map(v => 'yt' + v + ` <url> <without message>`)
-handler.tags = ['downloader']
-handler.command = /^yt(a|mp3)$/i
-
-handler.register = true
-handler.limit = true
-
-export default handler
-*/
-
-import { 
-    youtubedl,
-    youtubedlv2 
-} from '@bochilteam/scraper'
-
+import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
 var handler = async (m, { conn, args }) => {
-  if (!args[0]) throw 'Urlnya Mana Banh? >:('
+  if (!args[0]) throw 'Urlnya Mana Bang? >:('
   let q = '128kbps'
   let v = args[0]
 
@@ -90,8 +16,7 @@ var handler = async (m, { conn, args }) => {
   const info = `
 ● Judul: ${ttl}
 ● Ukuran: ${size}
-● Link YouTube: ${v}
-● Credits by Xnuvers007, https://github.com/Xnuvers007`
+● Link YouTube: ${v}`
 
   // Kirim pesan dan file audio ke user
   await conn.sendMessage(m.chat, { 
@@ -105,5 +30,5 @@ var handler = async (m, { conn, args }) => {
 // Jika ingin menambahkan tag, ubah code berikut:
 handler.tags = ['downloader']
 handler.help = ['ytmp3 <link>']
-handler.command = /^yta|ytmp3|youtubemp3$/i
+handler.command = /^ytmp3$/i
 export default handler
--- plugins/youtube-ytv.js
+++ plugins/youtube-ytv.js
@@ -1,69 +1,7 @@
-/*
-let limit = 80
-import fetch from 'node-fetch'
-import { youtubeSearch, youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper';
-let handler = async (m, { conn, args, isPrems, isOwner }) => {
-  if (!args || !args[0]) throw 'Uhm... urlnya mana?'
-  let chat = global.db.data.chats[m.chat]
-  const isY = /y(es)/gi.test(args[1])
-  const { thumbnail, video: _video, title} = await youtubedl(args[0]).catch(async _ => await youtubedlv2(args[0])).catch(async _ => await youtubedlv3(args[0]))
-  const limitedSize = (isPrems || isOwner ? 99 : limit) * 1024
-  let video, source, res, link, lastError, isLimit
-  for (let i in _video) {
-    try {
-      video = _video[i]
-      isLimit = limitedSize < video.fileSize
-      if (isLimit) continue
-      link = await video.download()
-      if (link) res = await fetch(link)
-      isLimit = res?.headers.get('content-length') && parseInt(res.headers.get('content-length')) < limitedSize
-      if (isLimit) continue
-      if (res) source = await res.arrayBuffer()
-      if (source instanceof ArrayBuffer) break
-    } catch (e) {
-      video = source = link = null
-      lastError = e
-    }
-  }
-  if ((!(source instanceof ArrayBuffer) || !link || !res.ok) && !isLimit) throw 'Error: ' + (lastError || 'Can\'t download video')
-  if (!isY && !isLimit) await conn.sendFile(m.chat, thumbnail, 'thumbnail.jpg', `
-*${htki} YOUTUBE ${htka}*
-
-*${htjava} Title:* ${title}
-*${htjava} Status:* Video sedang diproses...
-`.trim(), m)
-  let _thumb = {}
-  try { _thumb = { thumbnail: await (await fetch(thumbnail)).buffer() } }
-  catch (e) { }
-  if (!isLimit) await conn.sendFile(m.chat, link, title + '.mp4', `
-*${htki} YOUTUBE ${htka}*
-
-*${htjava} Title:* ${title}
-*${htjava} Quality:* 1080p
-*${htjava} Format:* mp4
-`.trim(), m, false, {
-    ..._thumb,
-    asDocument: chat.useDocument
-  })
-}
-handler.help = ['ytmp4 <url> <without message>']
-handler.tags = ['downloader']
-handler.command = /^yt(v|mp4)?$/i
-
-handler.exp = 0
-handler.register = false
-handler.limit = true
-
-
-export default handler
-*/
-
-// Update by Xnuvers007
-
 import { youtubedlv2, youtubedlv3 } from '@bochilteam/scraper'
 
 const handler = async (m, { conn, args, command }) => {
-  if (!args || !args[0]) throw 'Uhm... urlnya mana?'
+  if (!args[0]) throw 'Where`s Url?' // Zod
   const v = args[0]
 
   const resolutions = ["144p", "240p", "360p", "480p", "720p", "1080p"]
@@ -105,7 +43,7 @@ const handler = async (m, { conn, args, command }) => {
 
     await conn.sendMessage(m.chat, { video: { url: dlUrl, caption: title, ...thumb } }, { quoted: m })
 
-    await m.reply(`● Title: ${title}
+    await m.reply(`▢ Title: ${title}
 ● Resolution: ${selectedResolution}
 ● Size: ${size}
 ● Video telah berhasil diunduh!`)
@@ -114,8 +52,8 @@ const handler = async (m, { conn, args, command }) => {
   }
 }
 
-handler.command = /^(ytmp4|youtubemp4)$/i
 handler.help = ["ytmp4 <link>"]
 handler.tags = ['downloader']
+handler.command = /^(ytmp4)$/i
 
 export default handler

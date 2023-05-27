da9f05557eec4cdbc02b044d36afa85ac6ee70b2
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-05-27 13:22:35 +0700

    shortlink

--- plugins/owner-website.js
+++ plugins/owner-website.js
@@ -3,17 +3,16 @@ let handler = async (m, { conn }) => {
   let caption = `
 *「 Owner Website 」*
 
-Main Pages -> ryzn.my.id
-Blogspot -> www.win-tweak.my.id
-Uptime -> uptime.ryzn.my.id
+Main Pages -> https://ouo.io/oMI6P6
+Blogspot -> https://ouo.io/4Ph7Fj
+Uptime -> https://ouo.io/J105N
 
 ----------------------
-Prosekai Sticker Maker -> sekai.ryzn.my.id
+Prosekai Sticker Maker -> https://ouo.io/TDd2Bk
 ----------------------
 
-Revanced -> ryzn.my.id/revanced
-Spotify Mod -> ryzn.my.id/spotify
-Mod Manager -> ryzn.my.id/mods
+Revanced -> https://ouo.io/M6eHv4
+Spotify Mod -> https://ouo.io/a9QdDV3
 `.trim()
   m.reply(caption)
 }

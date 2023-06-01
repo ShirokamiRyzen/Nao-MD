65fecee412284924299e23fe646324e9e7c70ec7
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-06-02 06:23:03 +0700

    ..

--- plugins/image-gimage.js
+++ plugins/image-gimage.js
@@ -1,10 +1,11 @@
 import { googleImage } from '@bochilteam/scraper'
+
 var handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!text) throw `Use example ${usedPrefix}${command} Minecraft`
     const res = await googleImage(text)
     let image = res.getRandom()
     let link = image
-    conn.sendFile(m.chat, link, 'google.jpg', `*${htki} ɢᴏᴏɢʟᴇ ɪᴍᴀɢᴇ ${htka}*
+    conn.sendFile(m.chat, link, 'google.jpg', `*${htki} Google Image ${htka}*
 🔎 *Result:* ${text}
 🌎 *Source:* Google
 `,m)

21656e4e8004dba2112797cfc51721fc6e31d3a7
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-06-11 13:23:05 +0700

    .

--- plugins/sticker-attp.js
+++ plugins/sticker-attp.js
@@ -1,15 +1,3 @@
-/*
-let handler = async (m, { conn, text }) => {
-    let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
-    conn.sendFile(m.chat, global.API('xteam', '/attp', { file: '', text: teks }), 'attp.webp', '', m, false, { asSticker: true })
-}
-handler.help = ['attp <teks>']
-handler.tags = ['sticker']
-
-handler.command = /^attp$/i
-
-export default handler
-*/
 let handler = async(m, { conn, text, args, usedPrefix, command }) => {
     if (!text) throw `*CONTOH*\n${usedPrefix + command} Nao Tomori`
     let teks = encodeURI(text)

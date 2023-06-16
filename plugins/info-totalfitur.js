0c751a73368a56a785eb7c2eaeace9ebb33a8493
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-06-16 18:21:23 +0700

    status serv

--- plugins/info-totalfitur.js
+++ plugins/info-totalfitur.js
@@ -1,21 +1,9 @@
-import fs from 'fs'
 let handler = async (m, { conn, args, command }) => {
-let totalf = Object.values(global.plugins).filter(
+  let totalf = Object.values(global.plugins).filter(
     (v) => v.help && v.tags
-  ).length;
- await conn.sendButton(m.chat, `Total Fitur Bot Saat ini: ${totalf}\n`,wm + '\n\n' + botdate, giflogo, [['MENU','.menu']], m, {
-contextInfo: { externalAdReply :{
-                        mediaUrl: '',
-                        mediaType: 2,
-                        description: 'anu',
-                        title: bottime,
-                        body: 'Total Cintaku Padamu',          previewType: 0,
-                        thumbnail: fs.readFileSync("./thumbnail.jpg"),
-                        sourceUrl: sig
-                      }}
-})
+  ).length
+  conn.reply(m.chat, `Total Fitur saat ini: ${totalf}`, m)
 }
-
 
 handler.help = ['totalfitur']
 handler.tags = ['info']

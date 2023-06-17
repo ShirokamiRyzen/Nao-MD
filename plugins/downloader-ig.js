3b171f2b2e120d5454dd37124013880b3409cbdc
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-06-17 07:18:38 +0700

    terpaksa

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -1,19 +1,14 @@
-var handler = async (m, { args }) => {
-    if (!args[0]) throw 'Input URL'
-    try { 
-    	let res = await bochil.instagramdlv2(args[0]) 
-    let media = await res[0].url
-    if (!res) throw 'Can\'t download the post'
-    conn.sendMessage(m.chat, { video : { url : media }}, m) 
-    } catch {
-     try {
-     	let res2 = await bochil.instagramdlv3(args[0]) 
-   let media2 = res2.url
-   let cap = res2.title
-     return this.sendFile(m.chat, media, 'instagram.mp4', cap, m)
-     } finally {
-   }
-  }
+import axios from 'axios'
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  const linknya = args[0]
+
+  if (!args[0]) throw `Input *URL*`
+  if (!args[0].match(/https:\/\/www.instagram.com\/(p|reel|tv)/gi)) throw `*Link salah! Perintah ini untuk mengunduh postingan ig/reel/tv`
+  let api = await axios.get(`https://xzn.wtf/api/igdl?url=${linknya}&apikey=ikyy`)
+  let wm = `${global.wm}`
+  await m.reply('Sedang diproses...')
+  for (let e of api.data.media)
+    await conn.sendFile(m.chat, e, '', wm, m)
 }
 
 handler.help = ['ig'].map(v => v + ' <url>')

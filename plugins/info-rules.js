ee7bf0d9b508e87b79a6c4265016045db48e8320
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-06-17 08:14:12 +0700

    bersih bersih

--- plugins/info-rules.js
+++ plugins/info-rules.js
@@ -1,17 +1,7 @@
-import fs from 'fs'
-import moment from 'moment-timezone'
-let handler = async (m, { conn, usedPrefix, __dirname, text, command }) => {
-let tag = `@${m.sender.replace(/@.+/, '')}`
-  let mentionedJid = [m.sender]
-let name = conn.getName(m.sender)
-let esce = `text nya `
-let flaaa2 = [
-'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&script=water-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text=',
-'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&text=',
-'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text=',
-'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=',
-'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text=']
-let rules = `➯ *TIDAK SPAM BOT*
+let handler = async(m, { conn }) => {
+  var txt = `
+*「 Rules 」*
+➯ *TIDAK SPAM BOT*
  ⟬Kena BAN? chat owner⟭
 ➯ *Jangan menjual nomer BOT!!*
 ⟬Ini project open source jangan perjualbelikan⟭
@@ -25,49 +15,12 @@ let rules = `➯ *TIDAK SPAM BOT*
 ⟬Kontak Owner⟭
 ➯ *Tidak melayani Chat aneh-aneh*
 ⟬Buang waktu⟭
-❏┳━━◩
-┍┛
-┆⟥⟤ ➠ (${global.bottime})
-└─┈⟅`
-let nth = `☰⟥⟝⟞⟝❨ *Rᴜʟᴇs Mʏ Bᴏᴛ* ❩⟞⟝⟞⟤☰`
-conn.send3ButtonImg(m.chat, `${pickRandom(flaaa2)}` + `${ucapan()} ` + `${name}`, nth, rules, 'Menu', '.menu', 'Owner', '.owner', 'Credit', '.credit', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: 'https://wa.me/p/5489299044451325/6281387307198',
-    mediaType: 2, 
-    description: sgc,
-    title: "Sᴇᴡᴀ Cᴇᴋ Dɪsɪɴɪ Cᴜʏ!!!",
-    body: wm,
-    thumbnail: fs.readFileSync('./media/qr.png'),
-    sourceUrl: 'https://wa.me/p/5489299044451325/6281387307198',
-     }}
-  })
+`
+conn.sendMessage(m.chat,{ image :{ url : "https://telegra.ph/file/ad1ee4417fdb3cd9652f2.jpg" } , caption : txt }, { quoted: m })
 }
+
 handler.help = ['rules']
 handler.tags = ['info']
 handler.command = /^(rules)$/i
 
 export default handler
-
-function ucapan() {
-  const time = moment.tz('Asia/Jakarta').format('HH')
-  let res = "Selamat Malam"
-  if (time >= 4) {
-    res = "Selamat Pagi"
-  }
-  if (time >= 10) {
-    res = "Selamat Siang"
-  }
-  if (time >= 15) {
-    res = "Selamat Sore"
-  }
-  if (time >= 18) {
-    res = "Selamat Malam"
-  }
-  return res
-}
-
-function pickRandom(list) {
-  return list[Math.floor(Math.random() * list.length)]
-}
-
-const more = String.fromCharCode(8206)
-const readMore = more.repeat(4001)

59767201ddb583b7406e215628bfa33eb257c91b
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-06-18 18:11:49 +0700

    yang versi lama lebih waras

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -1,3 +1,4 @@
+/*
 import axios from 'axios'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
   const linknya = args[0]
@@ -9,6 +10,26 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   await m.reply('Sedang diproses...')
   for (let e of api.data.media)
     await conn.sendFile(m.chat, e, '', wm, m)
+}
+*/
+import fetch from 'node-fetch'
+var handler = async (m, { args }) => {
+    if (!args[0]) throw 'Input URL'
+    try { 
+    	let res = await bochil.instagramdlv2(args[0]) 
+    let media = await res[0].url
+    if (!res) throw 'Can\'t download the post'
+    m.reply('_In progress, please wait..._')
+    conn.sendMessage(m.chat, { video : { url : media }}, m) 
+    } catch {
+     try {
+     	let res2 = await bochil.instagramdlv3(args[0]) 
+   let media2 = res2.url
+   let cap = res2.title
+     return this.sendFile(m.chat, media, 'instagram.mp4', cap, m)
+     } finally {
+   }
+  }
 }
 
 handler.help = ['ig'].map(v => v + ' <url>')

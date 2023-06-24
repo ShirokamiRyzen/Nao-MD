341439bdf6e5f06ee1d9550728fbce85a8b02f9c
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-06-24 13:19:48 +0700

    hmm

--- plugins/anime-jadianime.js
+++ plugins/anime-jadianime.js
@@ -1,54 +1,24 @@
-import uploadImage from '../lib/uploadImage.js'
-import { JadiAnime } from 'jadianime-ts'
-import fs from 'fs'
-import crypto from 'crypto'
 import fetch from 'node-fetch'
+import uploadImage from '../lib/uploadImage.js'
 
-let handler = async (m, { conn, usedPrefix, command }) => {
-	let q = m.quoted ? m.quoted : m
-	let mime = (q.msg || q).mimetype || q.mediaType || ''
-	let anu, buffer, c = `*Jadi Anime*`
-	if (/image/g.test(mime) && !/webp/g.test(mime)) {
-		try {
-			let img = await q.download?.()
-			let id = 'jDa' + crypto.randomBytes(15).toString('hex')
-			let path = `./tmp/${id}.jpg`
-			buffer = Buffer.from(img)
-			await fs.writeFile(path, buffer, (err) => { if (err) throw err })
-			let image = await JdiAnime(path)
-			await conn.sendMessage(m.chat, { image: { url: image.img }, caption: c }, { quoted: m })
-		} catch (e) {
-			console.log(e)
-			let img = await q.download?.()
-			let out = await uploadImage(img)
-			try {
-				anu = await fetch(`https://api.lolhuman.xyz/api/imagetoanime?apikey=${global.lolkey}&img=${out}`)
-				buffer = Buffer.from(await anu.arrayBuffer())
-				if (Buffer.byteLength(buffer) < 100) throw Error('error, no buffer')
-				await conn.sendMessage(m.chat, { image: buffer, caption: c }, { quoted: m })
-			} catch (e) {
-				console.log(e)
-				try {
-				anu = await fetch(`https://api.lolhuman.xyz/api/imagetoanime?apikey=${global.lolkey}&img=${out}`)
-				buffer = Buffer.from(await anu.arrayBuffer())
-				if (Buffer.byteLength(buffer) < 100) throw Error('error, no buffer')
-				await conn.sendMessage(m.chat, { image: buffer, caption: c }, { quoted: m })
-				} catch (e) {
-					console.log(e)
-					m.reply(`[ ! ] Gagal, gunakan foto lainnya.`)
-				}
-			}
-		}
-	} else {
-		m.reply(`Kirim gambar dengan caption *${usedPrefix + command}* atau tag gambar yang sudah dikirim`)
-	}
+let handler = async (m, { conn, usedPrefix, command, text }) => {
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let name = await conn.getName(who)
+let q = m.quoted ? m.quoted : m
+let mime = (q.msg || q).mimetype || ''
+if (!mime) throw 'Kirim/Reply Gambar dengan caption .toanime'
+m.reply('Tunggu Sebentar...')
+let media = await q.download()
+let url = await uploadImage(media)
+let hasil = await (await fetch(`https://xzn.wtf/api/toanime?url=${url}&apikey=${global.xzn}`)).buffer()
+await conn.sendFile(m.chat, hasil, '', global.wm, m)
+	
 }
-
-handler.help = ['jadianime']
+handler.help = ['toanime']
 handler.tags = ['anime', 'ai']
-handler.command = /^(jadianime)$/i
+handler.command = /^(toanime)$/i
 
+handler.register = true
 handler.limit = true
-handler.premium = false
 
 export default handler

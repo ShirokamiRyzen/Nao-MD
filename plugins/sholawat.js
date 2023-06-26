2b117cc62a48f1f84700838e859554633f62bb0a
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-06-27 00:35:38 +0700

    fitur gapenting

--- plugins/sholawat.js
+++ plugins/sholawat.js
@@ -6,10 +6,10 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
 
 conn.reply(m.chat, info, m, { quoted: fkontak },{ contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow",
+    mediaUrl: "https://www.youtube.com/channel/UC5z1Xp6vQGU038B1RbMC3Gg",
     mediaType: 2,
-    description: "https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow", 
-    title: 'whmods-ᴍᴜʟᴛɪᴅᴇᴠɪᴄᴇ',
+    description: "https://www.youtube.com/channel/UC5z1Xp6vQGU038B1RbMC3Gg", 
+    title: 'Nao-Botz',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sig  }}})

312d76933104e6bbd86c91373c9098dc2009751c
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-06-27 16:47:40 +0700

    modified:   plugins/sfw.js

--- plugins/sfw.js
+++ plugins/sfw.js
@@ -3,35 +3,9 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, usedPrefix, command, text, args }) => {
   if (!args[0]) throw `select tag:
 awoo
-bite
-blush
-bonk
-bully
-cringe
-cry
-cuddle
-dance
-glomp
-handhold
-happy
-highfive
-hug
-kick
-kill
-kiss
-lick
 megumin
 neko
-nom
-pat
-poke
 shinobu
-slap
-smile
-smug
-wave
-wink
-yeet
 waifu`
   let res = await fetch(`https://api.waifu.pics/sfw/${text}`)
   if (!res.ok) throw await res.text()

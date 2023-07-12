a51f338000b8e1e67faf47446be42a4861e4e0bb
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-07-12 10:27:05 +0700

    iyh

--- plugins/anime-fanart.js
+++ plugins/anime-fanart.js
@@ -2,11 +2,12 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn, command }) => {
 	let url = `https://api.lolhuman.xyz/api/random/art?apikey=${global.lolkey}`
-	conn.sendButton(m.chat, 'Nih FanArtnya ', wm, await(await fetch(url)).buffer(), [['Next',`.${command}`]],m)
+	conn.sendFile(m.chat, await(await fetch(url)).buffer(), 'nih fanartnya', wm, m)
 }
 handler.command = /^(fanart)$/i
 handler.tags = ['anime']
 handler.help = ['fanart']
 handler.limit = true
-handler.premium = true
+handler.premium = false
+handler.register = true
 export default handler
--- plugins/anime-husbu.js
+++ plugins/anime-husbu.js
@@ -2,10 +2,12 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn, command }) => {
 	let url = `https://api.lolhuman.xyz/api/random/husbu?apikey=${global.lolkey}`
-	conn.sendButton(m.chat, 'Bojone Gepenk🐦', wm, await(await fetch(url)).buffer(), [['Next',`.${command}`]],m)
+	conn.sendFile(m.chat, await(await fetch(url)).buffer(), 'huu gepenk', wm, m)
 }
 handler.command = /^(husbu)$/i
 handler.tags = ['anime']
 handler.help = ['husbu']
 handler.limit = true
+handler.premium = false
+handler.register = true
 export default handler

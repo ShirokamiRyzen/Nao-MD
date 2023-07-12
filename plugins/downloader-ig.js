c27b670178882c3065e753ca74413b55cef0363e
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-07-12 16:02:16 +0700

    modified:   plugins/downloader-ig.js

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -14,8 +14,8 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 */
 import { instagramdl, instagramdlv2, instagramdlv3, instagramdlv4 } from '@bochilteam/scraper'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
+  if (!args[0]) throw `Urlnya?`
   try {
-    if (!args[0]) throw `Urlnya?`
     await m.reply('Sedang diproses...')
     const results = await instagramdl(args[0])
       .catch(async _ => await instagramdlv2(args[0]))

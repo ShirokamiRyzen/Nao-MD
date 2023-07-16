8a7a1567b798d1c1db53282f76f6f5cf000242ee
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-07-16 17:03:25 +0700

    new file:   plugins/ai-animediff2.js

---
+++ plugins/ai-animediff2.js
@@ -0,0 +1,25 @@
+import fetch from "node-fetch"
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+    let wm = global.wm
+
+    if (!text) throw `This command generates image from texts\n\n Example usage\n${ usedPrefix + command } 1girl, blush, megane, school uniform`
+        await m.reply('*Processing image*')
+        try {
+        let url = `https://api.xyroinee.xyz/api/ai/animediffusion2?q=${text}&apikey=${global.xyro}`
+        await conn.sendFile(m.chat, await(await fetch(url)).buffer(), 'image.jpg', wm, m)
+    } catch (e) {
+        console.log(e)
+        m.reply('failed')
+    }
+    
+}
+
+
+handler.help = ['animediff2 <prompt>']
+handler.tags = ['ai']
+handler.command = /^(animediff2)$/i
+handler.limit = true
+handler.register = true
+
+export default handler

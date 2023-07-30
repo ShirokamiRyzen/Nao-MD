54256cb6bdd61b9dfdaedef160b335b84dd44b4a
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-07-30 18:49:23 +0700

    modified:   plugins/downloader-aio.js

--- plugins/downloader-aio.js
+++ plugins/downloader-aio.js
@@ -1,45 +1,19 @@
-import axios from 'axios'
-import fs from 'fs'
+import fetch from 'node-fetch'
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  const linknya = args[0]
 
-const handler = async (m, { conn, args, command }) => {
-  if (!args[0]) throw 'Where\'s the URL?';
-  const userUrl = args[0];
-
-  const apiUrl = `https://xzn.wtf/api/download?url=${encodeURIComponent(userUrl)}&apikey=${global.xzn}`;
-
+  if (!args[0]) throw `Input *URL*`
   try {
-    const response = await axios.get(apiUrl);
-    const videoUrl = response.data.url[0].url;
-
-    await m.reply(`Downloading video from ${videoUrl}`);
-
-    const videoResponse = await axios({
-      method: 'GET',
-      url: videoUrl,
-      responseType: 'stream',
-    });
-
-    const videoName = `video-${Date.now()}.mp4`;
-    const videoPath = `./${videoName}`;
-
-    const writer = fs.createWriteStream(videoPath);
-    videoResponse.data.pipe(writer);
-
-    writer.on('finish', async () => {
-      await conn.sendFile(m.chat, fs.readFileSync(videoPath), videoName, '', m);
-
-      fs.unlinkSync(videoPath);
-
-      m.reply('Video successfully downloaded and sent!');
-    });
-
-    writer.on('error', (err) => {
-      m.reply(`Error occurred while downloading the video: ${err.message}`);
-    });
-  } catch (error) {
-    m.reply(`Error occurred: ${error.message}`);
+    let p = await fetch(`https://xzn.wtf/api/download?url=${linknya}&apikey=${global.xzn}`)
+    let v = await p.json()
+    let o = v.url[0].url
+    await m.reply('Sedang diproses...')
+    await conn.sendFile(m.chat, o, '', global.wm, m)
+  } catch (e) {
+    console.log(e)
+    m.reply(`Fitur error atau Otak pengguna error`)
   }
-};
+}
 
 handler.help = ['aiodl'].map((v) => v + ' <url>')
 handler.tags = ['downloader']

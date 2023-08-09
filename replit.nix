334a801db8dad9b51967145a58bcdf3a3ce250c0
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-08-09 15:55:55 +0700

    update replit

---
+++ replit.nix
@@ -0,0 +1,17 @@
+{ pkgs }: {
+	deps = [
+		pkgs.neofetch
+		pkgs.imagemagick
+		pkgs.nodejs-19_x
+        pkgs.nodePackages.typescript
+		pkgs.speedtest-cli
+		pkgs.jellyfin-ffmpeg
+		pkgs.git
+		pkgs.python2
+		pkgs.python310Packages.python
+        pkgs.libwebp
+        pkgs.wget
+        pkgs.yarn
+        pkgs.libuuid
+	];
+}

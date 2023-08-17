5f42a50de2b6f84c35aa25ba7795eaf09ba38d95
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-08-17 10:13:19 +0700

    gatau dah

--- plugins/youtube-yta.js
+++ plugins/youtube-yta.js
@@ -1,38 +1,25 @@
-import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
-var handler = async (m, { conn, args }) => {
-  if (!args[0]) throw 'Urlnya Mana Bang? >:('
-  let q = '128kbps'
-  let v = args[0]
+import fetch from 'node-fetch'
 
-  await m.reply('Permintaan download audio/mp3 youtube sedang diproses, mohon bersabar...')
+let handler = async (m, { conn, command, args, usedPrefix }) => {
+  const linknya = args[0]
 
-  // Ambil info dari video
-  const yt = await youtubedl(v).catch(async () => await  youtubedlv2(v))
-  const dl_url = await yt.audio[q].download()
-  const ttl = await yt.title
-  const size = await yt.audio[q].fileSizeH
-
-  // Tampilkan informasi file beserta thumbnail
-  const info = `
-● Judul: ${ttl}
-● Ukuran: ${size}
-● Link YouTube: ${v}`
-
-  // Kirim pesan dan file audio ke user
-  await conn.sendMessage(m.chat, { 
-    document: { url: dl_url }, 
-    mimetype: 'audio/mpeg', 
-    fileName: `${ttl}.mp3`,
-    caption: info
-  }, {quoted: m})
+  if (!args[0]) throw `Input *URL*`
+  m.reply(wait)
+    let res = await fetch(`https://api.ryzendesu.com/api/dowloader/ytmp3?url=${linknya}&apikey=${global.ryzen}`)
+    let result = await res.json()
+    let audio = result.result.link
+    //let audio = result.data.audio
+    let cap = global.wm
+    conn.sendMessage(m.chat, { document: { url: audio }, caption: cap }, m)
+    //conn.sendMessage(m.chat, { audio: { url: audio }, mimetype: 'audio/mp4' }, { quoted : m })
+  
 }
 
-// Jika ingin menambahkan tag, ubah code berikut:
+handler.help = ['ytmp3'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.help = ['ytmp3 <link>']
-handler.command = /^ytmp3$/i
-
-handler.register = true
 handler.limit = true
+handler.register = true
+
+handler.command = /^ytmp3$/i
 
 export default handler
--- plugins/youtube-ytv.js
+++ plugins/youtube-ytv.js
@@ -1,56 +1,25 @@
-import { youtubedlv2, youtubedlv3 } from '@bochilteam/scraper'
+import fetch from 'node-fetch'
 
-const handler = async (m, { conn, args, command }) => {
-  const v = args[0]
+let handler = async (m, { conn, command, args, usedPrefix }) => {
+  const linknya = args[0]
 
-  const resolutions = ["144p", "240p", "360p", "480p", "720p", "1080p"]
-  let qu = args[1] && resolutions.includes(args[1]) ? args[1] : "360p"
-  let q = qu.replace('p', '')
-
-  await m.reply('Permintaan download video/mp4 youtube sedang diproses, mohon bersabar...')
-
-  let yt
-  try {
-    yt = await youtubedlv2(v)
-  } catch {
-    yt = await youtubedlv3(v)
-  }
-
-  const title = await yt.title
-
-  let size = ''
-  let dlUrl = ''
-  let selectedResolution = ''
-  let selectedQuality = ''
-  for (let i = resolutions.length - 1; i >= 0; i--) {
-    const res = resolutions[i]
-    if (yt.video[res]) {
-      selectedResolution = res
-      selectedQuality = res.replace('p', '')
-      size = await yt.video[res].fileSizeH
-      dlUrl = await yt.video[res].download()
-      break
-    }
-  }
-
-  if (dlUrl) {
-
-    await conn.sendMessage(m.chat, { video: { url: dlUrl, caption: title, ...thumb } }, { quoted: m })
-
-    await m.reply(`● Title: ${title}
-● Resolution: ${selectedResolution}
-● Size: ${size}
-● Video Telah Berhasil Diunduh!`)
-  } else {
-    await m.reply(`Maaf, Video Tidak Ada.`)
-  }
+  if (!args[0]) throw `Input *URL*`
+  m.reply(wait)
+    let res = await fetch(`https://api.ryzendesu.com/api/dowloader/ytmp4?url=${linknya}&apikey=${global.ryzen}`)
+    let result = await res.json()
+    let video = result.result.link
+    //let audio = result.data.audio
+    let cap = global.wm
+    conn.sendMessage(m.chat, { video: { url: video }, caption: cap }, m)
+    //conn.sendMessage(m.chat, { audio: { url: audio }, mimetype: 'audio/mp4' }, { quoted : m })
+  
 }
 
-handler.help = ["ytmp4 <link>"]
+handler.help = ['ytmp4'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /(^ytmp4)$/i
-
-handler.register = true
 handler.limit = true
+handler.register = true
+
+handler.command = /^ytmp4$/i
 
 export default handler

d66e80438e175b78b6dc46c6fd672de580c41a9e
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-08-20 10:34:36 +0700

    .

--- plugins/image-pinterest.js
+++ plugins/image-pinterest.js
@@ -1,35 +1,39 @@
-import { pinterest } from '@bochilteam/scraper';
-import sharp from 'sharp';
+import { pinterest } from '@bochilteam/scraper'
+import sharp from 'sharp'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw `Example use ${usedPrefix + command} minecraft`;
-  const json = await pinterest(text);
-  const imageUrl = json.getRandom().replace(/&amp;/g, '&');
-  
-  const response = await fetch(imageUrl);
-  const imageBuffer = await response.buffer();
-  
-  const maxWhatsAppWidth = 9999; // Lebar maksimum WhatsApp padahal 640 :V gw tambah 9999 :V
-  const metadata = await sharp(imageBuffer).metadata();
-  const imageWidth = metadata.width;
-  
-  let resizedImage;
-  if (imageWidth > maxWhatsAppWidth) {
-    resizedImage = await sharp(imageBuffer)
-      .resize({ width: maxWhatsAppWidth, withoutEnlargement: true })
-      .toBuffer();
-  } else {
-    resizedImage = imageBuffer;
-  }
-  
-  conn.sendFile(m.chat, resizedImage, 'pinterest.jpg', `
+  if (!text) throw `Example use ${usedPrefix + command} Nao Tomori`
+
+  const maxImages = 10; // ubah mau kirim berapa foto (pake angka)
+  for (let i = 0; i < maxImages; i++) {
+    const json = await pinterest(text);
+    const imageUrl = json.getRandom().replace(/&amp;/g, '&');
+
+    const response = await fetch(imageUrl);
+    const imageBuffer = await response.buffer();
+
+    const maxWhatsAppWidth = 9999; // Lebar maksimum WhatsApp
+    const metadata = await sharp(imageBuffer).metadata();
+    const imageWidth = metadata.width;
+
+    let resizedImage;
+    if (imageWidth > maxWhatsAppWidth) {
+      resizedImage = await sharp(imageBuffer)
+        .resize({ width: maxWhatsAppWidth, withoutEnlargement: true })
+        .toBuffer();
+    } else {
+      resizedImage = imageBuffer;
+    }
+
+    conn.sendFile(m.chat, resizedImage, 'pinterest.jpg', `
 *Hasil pencarian*
 ${text}
-`.trim(), m)
+`.trim(), m);
+  }
 }
 
 handler.help = ['pinterest <keyword>']
 handler.tags = ['internet']
-handler.command = /^(pinterest)$/i
+handler.command = /^(pinterest|pin)$/i
 
 export default handler
--- plugins/image-wallpaper.js
+++ plugins/image-wallpaper.js
@@ -1,12 +1,18 @@
 import { wallpaper } from '@bochilteam/scraper'
+
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw `Use example ${usedPrefix}${command} Minecraft`
-    const res = await wallpaper(text)
-    const img = res[Math.floor(Math.random() * res.length)]
-    conn.sendFile(m.chat, img, 'wallpaper.jpg', `Result from *${text}*`, m)
+  if (!text) throw `Use example ${usedPrefix}${command} Minecraft`;
+  
+  const maxWallpapers = 10; // ubah berapa gambar yang mau dikirim
+  for (let i = 0; i < maxWallpapers; i++) {
+    const res = await wallpaper(text);
+    const img = res[Math.floor(Math.random() * res.length)];
+    conn.sendFile(m.chat, img, 'wallpaper.jpg', `Result from *${text}*`, m);
+  }
 }
 
-handler.help = ['wallpaper <query>']
-handler.tags = ['internet']
-handler.command = /^wall(paper)?q?$/i
+handler.help = ['wallpaper' + ' <query>']
+handler.tags = ['downloader']
+handler.command = /^wallpaper$/i
+
 export default handler

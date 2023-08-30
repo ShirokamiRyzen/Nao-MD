4b0a484cf3954273046dc676e81cbf56bfce1e63
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-08-30 22:08:08 +0700

    modified:   plugins/image-pinterest.js

--- plugins/image-pinterest.js
+++ plugins/image-pinterest.js
@@ -1,15 +1,26 @@
+//By Shirokami Ryzen
+//Dont delete this credit!!!
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, command, text, usedPrefix }) => {
 
   let api = "api.ryzendesu.com"
-  if (!text) throw `Input *Query*`;
-  conn.reply(m.chat, 'Wait a moment...', m);
+  if (!text) throw `Input *Query*`
+  conn.reply(m.chat, 'Wait a moment...', m)
 
   try {
     let res = await fetch(`https://api.ryzendesu.com/api/search/pinterest?text=${text}&apikey=${global.ryzen}`);
     let result = await res.json();
-    let gambarUrls = result.result.slice(0, 10); // Ambil 10 gambar pertama
+    let gambarUrls = result.result;
+
+    // Acak urutan gambar menggunakan algoritma Fisher-Yates
+    for (let i = gambarUrls.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [gambarUrls[i], gambarUrls[j]] = [gambarUrls[j], gambarUrls[i]];
+    }
+
+    // Ambil 10 gambar pertama setelah diacak
+    gambarUrls = gambarUrls.slice(0, 10);
 
     for (let i = 0; i < gambarUrls.length; i++) {
       let imageUrl = gambarUrls[i];
@@ -22,10 +33,10 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
       // Tambahkan jeda agar tidak mengirim gambar terlalu cepat
       await new Promise(resolve => setTimeout(resolve, 500));
     }
-    m.reply(api)
+    m.reply(api);
   } catch (e) {
     console.log(e);
-    conn.reply(m.chat, 'Terjadi kesalahan saat mendownload gambar.', m);
+    conn.reply(m.chat, 'Terjadi kesalahan saat mendownload gambar.', m)
   }
 }
 

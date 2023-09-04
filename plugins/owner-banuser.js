2332a4239da3a6933481d4be5f3848b3d6a811a7
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-09-04 22:40:43 +0700

    fix banned user handler

--- plugins/owner-banuser.js
+++ plugins/owner-banuser.js
@@ -1,12 +1,21 @@
 let handler = async (m, { conn, text }) => {
-    if (!text) throw 'Who wants to be banned ?'
+    if (!text) throw 'Who wants to be banned? Provide the user\'s phone number.'
     let who
-    if (m.isGroup) who = m.mentionedJid[0]
-    else who = m.chat
-    if (!who) throw 'Tag??'
+    if (m.isGroup) {
+        if (!m.mentionedJid) throw 'No user mentioned to ban.'
+        who = m.mentionedJid[0]
+    } else {
+        // Check if the input is a valid phone number
+        let phoneNumber = text.replace(/[^0-9]/g, '') // Remove non-numeric characters
+        who = phoneNumber + '@s.whatsapp.net'
+    }
     let users = global.db.data.users
-    users[who].banned = true
-    conn.reply(m.chat, 'Done!', m)
+    if (users[who]) {
+        users[who].banned = true
+        conn.reply(m.chat, 'Done!', m)
+    } else {
+        throw 'User not found.'
+    }
 }
 handler.help = ['ban']
 handler.tags = ['owner']

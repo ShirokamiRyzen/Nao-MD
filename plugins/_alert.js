b6caf96a6c1edf383bec3d22a2fe48e82b91d6b7
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-09-05 14:17:43 +0700

    modified:   plugins/_alert.js

--- plugins/_alert.js
+++ plugins/_alert.js
@@ -9,7 +9,7 @@ handler.before = async function (m) {
 
     // Check if the user is banned
     if (global.db.data.users[m.sender].banned === true) {
-        m.reply('Maaf, nomor Anda telah diblokir untuk menggunakan bot ini.');
+        m.reply('Maaf, nomor Anda telah diblokir untuk menggunakan bot ini.\n\nSorry, your number has been blocked from using this bot.');
         return;
     }
 }

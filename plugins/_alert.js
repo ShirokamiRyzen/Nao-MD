5b94a0ab025c42ba7136ec55e3bd5cdca9933391
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-09-05 14:16:37 +0700

    modified:   plugins/_alert.js

--- plugins/_alert.js
+++ plugins/_alert.js
@@ -1,11 +1,18 @@
 let handler = m => m
 
 handler.before = async function (m) {
+    // Check if the message is sent in a group
+    if (m.isGroup) {
+        // Mengabaikan pesan dalam grup
+        return;
+    }
+
     // Check if the user is banned
     if (global.db.data.users[m.sender].banned === true) {
-        m.reply('Sorry, Your number has been banned for using bot');
+        m.reply('Maaf, nomor Anda telah diblokir untuk menggunakan bot ini.');
         return;
     }
 }
+
 
 export default handler

c52abc4d0cd5e550ae8f38882e18bcc19ec3ccb0
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-09-17 08:26:45 +0700

    fix sticker

---
+++ plugins/sticker-qc.js
@@ -0,0 +1,55 @@
+import { sticker } from '../lib/sticker.js'
+import axios from 'axios'
+
+let handler = async (m, {
+    conn,
+    args,
+    usedPrefix,
+    command
+}) => {
+let text
+    if (args.length >= 1) {
+        text = args.slice(0).join(" ")
+    } else if (m.quoted && m.quoted.text) {
+        text = m.quoted.text
+    } else throw "Teksnya mana?\n\nContoh : .qc nanz"
+   if (!text) return m.reply('Teksnya mana?')
+   if (text.length > 10000) return m.reply('Maximal 10000 karakter!')
+    let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://i.ibb.co/3Fh9V6p/avatar-contact.png')
+
+   const obj = {
+      "type": "quote",
+      "format": "png",
+      "backgroundColor": "#000000",
+      "width": 512,
+      "height": 768,
+      "scale": 2,
+      "messages": [{
+         "entities": [],
+         "avatar": true,
+         "from": {
+            "id": 1,
+            "name": m.name,
+            "photo": {
+               "url": pp
+            }
+         },
+         "text": text,
+         "replyMessage": {}
+      }]
+   }
+   const json = await axios.post('https://bot.lyo.su/quote/generate', obj, {
+      headers: {
+         'Content-Type': 'application/json'
+      }
+   })
+   const buffer = Buffer.from(json.data.result.image, 'base64')
+   let stiker = await sticker(buffer, false, global.packname, global.author)
+    if (stiker) return conn.sendFile(m.chat, stiker, 'Quotly.webp', '', m)
+}
+
+handler.help = ['qc']
+handler.tags = ['sticker']
+handler.command = /^(qc)$/i
+
+export default handler

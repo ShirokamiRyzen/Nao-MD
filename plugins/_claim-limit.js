643270a606d0f67ebb06c7f5d49be60a6822e8db
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-10-11 22:43:36 +0700

    limit

--- plugins/_claim-limit.js
+++ plugins/_claim-limit.js
@@ -1,23 +1,31 @@
 const rewards = {
-    limit: 20,
+  limit: 20,
+}
+const cooldown = 86400000
+let handler = async (m,{ conn} ) => {
+  let user = global.db.data.users[m.sender]
+
+  if (user.role === 'Free user' && user.limit <= 100) {
+    conn.reply(m.chat, 'Free user only have 100 Limit max', m)
+    return
   }
-  const cooldown = 86400000
-  let handler = async (m,{ conn} ) => {
-    let user = global.db.data.users[m.sender]
-    if (new Date - user.lastclaim < cooldown) throw `You have already claimed this daily limit!, wait for *${((user.lastclaim + cooldown) - new Date()).toTimeString()}*`
-    let text = ''
-    for (let reward of Object.keys(rewards)) {
-      if (!(reward in user)) continue
-      user[reward] += rewards[reward]
-      text += `*+${rewards[reward]}* ${global.rpg.emoticon(reward)}${reward}\n`
-    }
-    conn.reply(m.chat, text.trim(), m)
-    user.lastclaim = new Date * 1
+
+  if (new Date - user.lastclaim < cooldown) throw `You have already claimed this daily limit!, wait for *${((user.lastclaim + cooldown) - new Date()).toTimeString()}*`
+  let text = ''
+  for (let reward of Object.keys(rewards)) {
+    if (!(reward in user)) continue
+    user[reward] += rewards[reward]
+    text += `*+${rewards[reward]}* ${global.rpg.emoticon(reward)}${reward}\n`
   }
-  handler.help = ['claimlimit']
-  handler.tags = ['main']
-  handler.command = /^(claimlimit)$/i
-  
-  handler.cooldown = cooldown
-  
-  export default handler
+  conn.reply(m.chat, text.trim(), m)
+  user.lastclaim = new Date * 1
+}
+
+handler.help = ['claimlimit']
+handler.tags = ['main']
+handler.command = /^(claimlimit)$/i
+
+handler.cooldown = cooldown
+handler.inactive = false
+
+export default handler

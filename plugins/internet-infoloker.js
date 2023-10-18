e2b28bab36a3fc1be9e83d64db135724c6a947d3
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-10-18 16:30:35 +0700

    ingfo lur

---
+++ plugins/internet-infoloker.js
@@ -0,0 +1,53 @@
+import cheerio from 'cheerio'
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, args, usedPrefix, text, command }) => {
+    if (!text) return m.reply("Input query\nExample: .infoloker programmer")
+    await m.reply(wait)
+    try {
+        let res = await infoloker(text);
+        // Limit the results to the first 5 items
+        res = res.slice(0, 5);
+        let teks = res.map((item, index) => {
+            return `
+🔍 *[ RESULT ${index + 1} ]*
+📰 *Title:* ${item.job || 'Tidak diketahui'}
+🏢 *Perusahaan:* ${item.perusahaan || 'Tidak diketahui'}
+📍 *Daerah:* ${item.daerah || 'Tidak diketahui'}
+🔗 *Link Detail:* ${item.link_Detail || 'Tidak diketahui'}
+⬆️ *Upload:* ${item.upload || 'Tidak diketahui'}
+`;
+        }).filter(v => v).join("\n\n________________________\n\n");
+        await m.reply(teks)
+    } catch (e) {
+        await m.reply(eror)
+    }
+};
+handler.help = ["infoloker"]
+handler.tags = ["internet"]
+handler.command = /^(infoloker)$/i
+handler.register = true
+
+export default handler
+
+/* New Line */
+async function infoloker(query) {
+    const url = `https://www.jobstreet.co.id/id/job-search/${query}-jobs/`;
+    const response = await fetch(url);
+    const html = await response.text();
+
+    const $ = cheerio.load(html);
+    const format = [];
+
+    $('article').each((a, article) => {
+        const job = $(article).find('h1 a div').text();
+        const perusahaan = $(article).find('span').eq(0).text();
+        const daerah = $(article).find('span span').text();
+        const link_Detail = 'https://www.jobstreet.co.id' + $(article).find('h1 a').attr('href');
+        const upload = $(article).find('div > time > span').text();
+
+        format.push({ job, perusahaan, daerah, upload, link_Detail });
+    });
+
+    return format;
+}

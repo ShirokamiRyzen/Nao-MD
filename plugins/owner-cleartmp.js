e724ebe67bf3bd0cec6eb1ed5634e62e85f2176a
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-11-10 18:04:46 +0700

    update replit

--- plugins/owner-cleartmp.js
+++ plugins/owner-cleartmp.js
@@ -1,30 +1,46 @@
 import { tmpdir } from 'os'
 import path, { join } from 'path'
-import {
-  readdirSync,
-  statSync,
-  unlinkSync,
-  existsSync,
-  readFileSync,
-  watch
-} from 'fs'
+import { readdirSync, statSync, unlinkSync, existsSync } from 'fs'
+
 let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
 
-conn.reply(m.chat, 'Done', m)
+  const tmp = [tmpdir(), join(__dirname, '../tmp')];
+  const filenames = [];
 
-const tmp = [tmpdir(), join(__dirname, '../tmp')]
-  const filename = []
-  tmp.forEach(dirname => readdirSync(dirname).forEach(file => filename.push(join(dirname, file))))
-  return filename.map(file => {
-    const stats = statSync(file)
-    unlinkSync(file)
-})
+  tmp.forEach(dirname => {
+    readdirSync(dirname).forEach(file => {
+      filenames.push(join(dirname, file));
+    });
+  });
+
+  const deletedFiles = [];
+
+  filenames.forEach(file => {
+    const stats = statSync(file);
+
+    if (stats.isDirectory()) {
+      console.log(`Skipping directory: ${file}`);
+    } else {
+      unlinkSync(file);
+      deletedFiles.push(file);
+    }
+  });
+
+  conn.reply(m.chat, 'Success!', m);
+
+  if (deletedFiles.length > 0) {
+    console.log('Deleted files:', deletedFiles);
+    conn.reply(m.chat, `Deleted files:\n${deletedFiles.join('\n')}`, m);
+  }
+
+  if (deletedFiles.length == 0) {
+    conn.reply(m.chat, 'Tidak ada file yang tersisa di tmp', m);
+  }
 }
 
 handler.help = ['cleartmp']
 handler.tags = ['owner']
-handler.command = /^(cleartmp)$/i
-
+handler.command = /^(cleartmp|clear|tmpclear|cleantmp)$/i
 handler.rowner = true
 
 export default handler

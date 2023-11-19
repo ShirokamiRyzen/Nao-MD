2381d5eef78896ac71565789312d8f639620f4e0
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-11-19 21:37:59 +0700

    fix

--- plugins/youtube-ytv.js
+++ plugins/youtube-ytv.js
@@ -7,43 +7,35 @@ import os from 'os'
 const streamPipeline = promisify(pipeline);
 
 var handler = async (m, { conn, command, text, usedPrefix }) => {
-
-
-await conn.sendMessage(m.chat, {
-        react: {
-            text: "⏳",
-            key: m.key,
-        }
-    })
-  if (!text) throw `Usage: ${usedPrefix}${command} <YouTube Video URL>`;
-
-  const videoUrl = text; // Gunakan URL video YouTube yang diberikan sebagai input
-
-  // Dapatkan informasi video dari URL
-  const videoInfo = await ytdl.getInfo(videoUrl);
-
-  // Ekstrak informasi yang diperlukan
-  const { videoDetails } = videoInfo;
-  const { title, thumbnails, lengthSeconds, viewCount, uploadDate } = videoDetails;
-  const thumbnail = thumbnails[0].url; // Gunakan thumbnail pertama
-
-
-  // Dapatkan aliran video dengan kualitas tertinggi
-  const videoStream = ytdl(videoUrl, {
-    quality: 'highestvideo',
+  await conn.sendMessage(m.chat, {
+    react: {
+      text: '⏳',
+      key: m.key,
+    },
   });
 
-  // Dapatkan path ke direktori sementara sistem
+  if (!text) throw `Usage: ${usedPrefix}${command} <YouTube Video URL>`;
 
-  // Buat writable stream dalam direktori sementara
+  const videoUrl = text;
+
+  const videoInfo = await ytdl.getInfo(videoUrl);
+
+  const { videoDetails } = videoInfo;
+  const { title, thumbnails, lengthSeconds, viewCount, uploadDate } = videoDetails;
+  const thumbnail = thumbnails[0].url;
+
+  const videoStream = ytdl(videoUrl, {
+    filter: 'audioandvideo', // Change here to filter audio and video
+    quality: 'highest', // You may want to adjust this based on the available qualities
+  });
+
   const writableStream = fs.createWriteStream(`tmp/${title}.mp4`);
 
-  // Mulai mengunduh video
   await streamPipeline(videoStream, writableStream);
 
   let doc = {
     video: {
-      url: `tmp/${title}.mp4`
+      url: `tmp/${title}.mp4`,
     },
     mimetype: 'video/mp4',
     fileName: `${title}`,
@@ -54,14 +46,13 @@ await conn.sendMessage(m.chat, {
         mediaUrl: videoUrl,
         title: title,
         sourceUrl: videoUrl,
-        thumbnail: await (await conn.getFile(thumbnail)).data
-      }
-    }
+        thumbnail: await (await conn.getFile(thumbnail)).data,
+      },
+    },
   };
 
   await conn.sendMessage(m.chat, doc, { quoted: m });
 
-  // Hapus file video
   fs.unlink(`tmp/${title}.mp4`, (err) => {
     if (err) {
       console.error(`Failed to delete video file: ${err}`);
@@ -69,11 +60,13 @@ await conn.sendMessage(m.chat, {
       console.log(`Deleted video file: tmp/${title}.mp4`);
     }
   });
-};
+}
 
 handler.help = ['ytmp4'].map((v) => v + ' <URL>')
 handler.tags = ['downloader']
 handler.command = /^(ytmp4)$/i
+
+handler.limit = true
 handler.register = true
 
 export default handler

fb3fdd6c7c7f4d23568d7f2ad45e67049afe7e07
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-11-19 21:55:22 +0700

    limit

--- plugins/youtube-yta.js
+++ plugins/youtube-yta.js
@@ -51,6 +51,8 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
 handler.help = ['ytmp3'].map((v) => v + ' <URL>')
 handler.tags = ['downloader']
 handler.command = /^(ytmp3)$/i
+
+handler.limit = true
 handler.register = true
 handler.exp = 0
 

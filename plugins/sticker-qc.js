8b2ff3cb21197cae128f2eddcc45e9bf1c480666
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-12-05 21:32:57 +0700

    cobain sendiri

--- plugins/sticker-qc.js
+++ plugins/sticker-qc.js
@@ -1,25 +1,84 @@
 import { sticker } from '../lib/sticker.js'
-import axios from 'axios'
+import uploadFile from '../lib/uploadFile.js'
+import fetch from 'node-fetch'
 
-const handler = async (m, { conn, args, usedPrefix, command }) => {
-   let text
-   if (args.length >= 1) {
-      text = args.slice(0).join(" ");
-   } else if (m.quoted && m.quoted.text) {
-      text = m.quoted.text
-   } else throw "Mana teksnya?"
-   if (!text) return m.reply('Mana teksnya?')
-   const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
-   const mentionRegex = new RegExp(`@${who.split('@')[0].replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}\\s*`, 'g');
-   const orang = text.replace(mentionRegex, '');
-   const pp = await conn.profilePictureUrl(who).catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
-   const number = await conn.getName(who)
-   const obj = { "type": "quote", "format": "png", "backgroundColor": "#000000", "width": 1024, "height": 1024, "scale": 2, "messages": [{ "entities": [], "avatar": true, "from": { "id": 1, "name": `${who?.name || number}`, "photo": { url: `${pp}` } }, "text": orang, "replyMessage": {} }] };
-   const json = await axios.post('https://bot.lyo.su/quote/generate', obj, { headers: { 'Content-Type': 'application/json' } });
-   const buffer = Buffer.from(json.data.result.image, 'base64');
-   let stiker = await sticker(buffer, false, global.stickpack, global.stickauth);
-   if (stiker) return conn.sendFile(m.chat, stiker, 'qc.webp', '', m);
+// Assuming this function converts an image URL to a buffer
+async function getBuffer(url) {
+  const response = await fetch(url);
+  const buffer = await response.buffer();
+  return buffer;
 }
+
+const handler = async (m, { conn, text, args }) => {
+   const pp = 'https://telegra.ph/file/ab2d8562be18ad26b1668.jpg';
+
+   if (!args[0] && !m.quoted) {
+      return m.reply(`Please provide a text (Type or mention a message) !`);
+   }
+
+   let userPfp;
+   if (m.quoted && !text) {
+      try {
+         userPfp = await conn.profilePictureUrl(m.sender, 'image');
+      } catch (e) {
+         userPfp = pp;
+      }
+   } else {
+      try {
+         userPfp = await conn.profilePictureUrl(m.sender, 'image');
+      } catch (e) {
+         userPfp = pp;
+      }
+   }
+
+   const trimtext = text.length > 50 ? `${text.substring(0, 50 - 3)}...` : text;
+   let trimqtext;
+   if (m.quoted && m.quoted.text) {
+      trimqtext = m.quoted.text.length > 50 ? `${m.quoted.text.substring(0, 50 - 3)}...` : m.quoted.text;
+   }
+
+   const q = m.quoted ? m.quoted : m;
+   const mime = (q.msg || q).mimetype || q.mediaType || '';
+   let media, img;
+
+   if (/image/.test(mime)) {
+      img = await q.download?.();
+      if (img) media = await uploadFile(img);
+   }
+
+   const tkw = !trimtext && m.quoted && m.quoted.text ? trimqtext : trimtext;
+   const qwe = trimtext && m.quoted && m.quoted.text ? { qname: m.quoted.name, qtext: trimqtext } : {};
+
+   const apiKey = global.xzn;
+
+   const queryParams = new URLSearchParams({
+      text: tkw,
+      username: !trimtext && m.quoted ? m.quoted.name : m.name,
+      avatar: await uploadFile(await getBuffer(userPfp)),
+      ...(media ? { media } : {}),
+      ...qwe,
+      apikey: apiKey,
+   });
+
+   const apiUrl = `https://skizo.tech/api/qc?${queryParams.toString()}`;
+
+   try {
+      const response = await fetch(apiUrl);
+      if (!response.ok) {
+         throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const arrayBuffer = await response.buffer();
+      const stiker = await sticker(arrayBuffer, global.packname, global.author);
+
+      if (stiker) {
+         return conn.sendFile(m.chat, stiker, 'Quotly.webp', '', m);
+      }
+   } catch (e) {
+      console.error(e);
+      return e.toString();
+   }
+};
 
 handler.help = ['qc']
 handler.tags = ['sticker']

0b8580d4b1f14777c39753646d0bb9241a93e3cb
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-12-05 20:44:10 +0700

    simplified

--- plugins/owner-addprem.js
+++ plugins/owner-addprem.js
@@ -1,32 +1,41 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    let who
-    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
-    else who = m.chat
-    let user = db.data.users[who]
-    if (!who) throw `tag or mention someone!`
-    let txt = text.replace('@' + who.split`@`[0], '').trim()
+    let user;
+    if (m.isGroup) {
+        user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false;
+    } else {
+        user = text.split(' ')[0];
+        user = user.replace('@', '') + '@s.whatsapp.net';
+    }
 
-    if (!txt) throw `where the number of days?`
-    if (isNaN(txt)) return m.reply(`only number!\n\nexample:\n${usedPrefix + command} @${m.sender.split`@`[0]} 7`)
+    let userData = db.data.users[user];
+    if (!userData) throw `User not found!`;
 
-    var jumlahHari = 86400000 * txt
-    var now = new Date() * 1
+    // Extract the user's phone number from the text
+    let phoneNumber = user.split('@')[0];
 
-    if (user.role === 'Free user') user.role = 'Premium user'
-    if (now < user.premiumTime) user.premiumTime += jumlahHari
-    else user.premiumTime = now + jumlahHari
-    user.premium = true
+    if (!phoneNumber) throw `where the number of days?`;
+    if (isNaN(phoneNumber)) return m.reply(`only number!\n\nexample:\n${usedPrefix + command} @${m.sender.split`@`[0]} 7`);
+
+    let txt = text.split(' ')[1]; // Extract the second part of the text (duration)
+
+    var jumlahHari = 86400000 * txt;
+    var now = new Date() * 1;
+
+    if (userData.role === 'Free user') userData.role = 'Premium user';
+    if (now < userData.premiumTime) userData.premiumTime += jumlahHari;
+    else userData.premiumTime = now + jumlahHari;
+    userData.premium = true;
 
     m.reply(`✔️ Success
-📛 *Name:* ${user.name}
+📛 *Name:* ${userData.name}
 📆 *Days:* ${txt} days
-📉 *Countdown:* ${user.premiumTime - now}`)
-}
-handler.help = ['addprem [@user] <days>']
+📉 *Countdown:* ${userData.premiumTime - now}`);
+};
+
+handler.help = ['addprem <phone number> <days>']
 handler.tags = ['owner']
 handler.command = /^addprem?$/i
 
-handler.group = true
 handler.rowner = true
 
 export default handler

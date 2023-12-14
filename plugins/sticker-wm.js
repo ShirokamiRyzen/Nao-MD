9a166ebba88296cfa80d3db510e7789d9c1af5d3
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-12-14 18:21:23 +0700

    awokawokawok

--- plugins/sticker-wm.js
+++ plugins/sticker-wm.js
@@ -23,6 +23,6 @@ let handler = async (m, { conn, text }) => {
 handler.help = ['wm <packname>|<author>']
 handler.tags = ['sticker']
 handler.command = /^wm$/i
-handler.premium = true
+handler.limit = 5
 
 export default handler

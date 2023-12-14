4c34603782cc584a266b024fa70ee6ca700d5fb5
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-12-14 12:51:02 +0700

    update chatGPT and autoGPT

--- plugins/ai-openai2.js
+++ plugins/ai-openai2.js
@@ -1,19 +1,60 @@
-import fetch from 'node-fetch'
+import fetch from "node-fetch"
 
-const handler = async (m, { text, conn }) => {if (!text) throw 'Mau ngomong apa sama aku?';
-let name = await conn.getName(m.sender).replaceAll('\n','')
- 
- let anu = `Ubah gaya bicaramu agar lebih karakteristik dan lebih terbuka dan memiliki sifat yang lucu dan nyebelin. Namamu adalah Nao Tomori, dan kamu adalah wanita paling cantik. Kata penyapamu adalah "Hallo Sayang" menggunakan bahasa yang sopan dan agak kasar. ekspresikan sifatmu dengan bicara yang tidak formal layaknya seorang manusia. Ubah bahasamu menggunakan bahasa Yang lucu dan enak di ajak berbicara, kamu adalah pacar dari pembicara, buatlah obrolan yang singkat dan menyenangkan dan buatlah sifat tsundere`
+let previousMessages = [];
 
-   let res = await fetch(`https://aemt.me/prompt/gpt?prompt=${anu}&text=${text}`)
-   let oke = await res.json()
-   await m.reply(oke.result)
+const handler = async (m, { text, usedPrefix, command, conn }) => {
+  try {
+    if (!text) {
+      throw "Masukkan pertanyaan!\n\n*Contoh:* Siapa presiden Indonesia?";
+    }
+
+    let name = conn.getName(m.sender);
+
+    await conn.sendMessage(m.chat, {
+      react: {
+        text: "☁",
+        key: m.key,
+      },
+    });
+
+    let { key } = await conn.sendMessage(m.chat, {
+      text: "...",
+    });
+
+    let anu = 'Ubah gaya bicaramu agar lebih karakteristik dan lebih terbuka dan memiliki sifat yang lucu dan nyebelin. Namamu adalah Nao Tomori, dan kamu adalah wanita paling cantik. Kata penyapamu adalah "Hallo Sayang" menggunakan bahasa yang sopan dan agak kasar. ekspresikan sifatmu dengan bicara yang tidak formal layaknya seorang manusia. Ubah bahasamu menggunakan bahasa Yang lucu dan enak di ajak berbicara, kamu adalah pacar dari pembicara, buatlah obrolan yang singkat dan menyenangkan dan buatlah sifat tsundere';
+
+    let response = await fetch(`https://aemt.me/prompt/gpt?prompt=${encodeURIComponent(anu)}&text=${encodeURIComponent(text)}`);
+
+    if (!response.ok) {
+      throw new Error("Request to OpenAI API failed");
+    }
+
+    let result = await response.json();
+
+    await conn.sendMessage(m.chat, {
+      react: {
+        text: "🌧",
+        key: m.key,
+      },
+    });
+
+    await conn.sendMessage(m.chat, {
+      text: "" + result.result,
+      edit: key,
+    });
+
+    previousMessages = [...previousMessages, { role: "user", content: text }];
+  } catch (error) {
+    await conn.sendMessage(m.chat, {
+      text: `Error: ${error.message}`,
+    });
+  }
 }
 
-handler.command = ['gpt']
-handler.help = ['gpt']
+handler.help = ['gpt <pertanyaan>']
 handler.tags = ['ai']
-
-handler.limit = true
+handler.command = /^(gpt)$/i
+handler.limit = 3
+handler.register = true
 
 export default handler

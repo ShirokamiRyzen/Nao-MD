9e31c529acdd79fd4285b7eb6d7d0eaf2600cf2c
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-12-22 16:49:04 +0700

    sementara

--- plugins/downloader-facebook.js
+++ plugins/downloader-facebook.js
@@ -1,4 +1,4 @@
-import axios from 'axios'
+/*import axios from 'axios'
 import fetch from 'node-fetch'
 
 const getBuffer = async (url) => {
@@ -13,7 +13,7 @@ var handler = async (m, { args }) => {
     }
     
     try {
-        const response = await axios.get(`https://backend.shirokamiryzen.repl.co/fb?u=${args[0]}`);
+        const response = await axios.get(`https://tr.deployers.repl.co/fb?u=${url}`);
         
         // Check if the response data is an array with at least one URL
         if (Array.isArray(response.data) && response.data.length > 0) {
@@ -30,6 +30,36 @@ var handler = async (m, { args }) => {
         console.log(e)
         conn.reply(m.chat, 'Invalid response format from the server', m)
       }
+}*/
+
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  const linknya = args[0];
+
+  if (!args[0]) throw `Input *URL*`;
+
+  try {
+    let response = await fetch(`https://api.ryzendesu.com/api/dowloader/fbdown?url=${linknya}&apikey=${global.ryzen}`);
+    let data = await response.json();
+
+    // Check if the status is true before proceeding
+    if (data.status) {
+      let videoUrl = data.result.HD || data.result.Normal_video;
+
+      if (videoUrl) {
+        await m.reply(wait);
+        await conn.sendFile(m.chat, videoUrl, '', global.wm, m);
+      } else {
+        throw 'HD and Normal video links not found in the API response.';
+      }
+    } else {
+      throw 'API returned false status';
+    }
+  } catch (e) {
+    console.error(e);
+    m.reply(`Error: ${e}`);
+  }
 }
 
 handler.help = ['fb <url>']

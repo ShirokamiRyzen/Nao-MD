20e50b9bcf1a19a930d496bb6b128d0e1313a405
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-12-25 16:23:08 +0700

    mass delete

--- plugins/owner-deluser.js
+++ plugins/owner-deluser.js
@@ -1,53 +1,49 @@
 let handler = async (m, { conn, text }) => {
-	function no(number) {
-		return number.replace(/\s/g, '').replace(/([@+-])/g, '')
-	}
+    function no(number) {
+        return number.replace(/\s/g, '').replace(/([@+-])/g, '');
+    }
 
-	text = no(text)
+    let numbers = text.split(/\s+/).map(no);
 
-	if (isNaN(text)) {
-		var number = text.split`@`[1]
-	} else if (!isNaN(text)) {
-		var number = text
-	}
+    if (!numbers.length && !m.quoted) {
+        return conn.reply(m.chat, `*❏ DELETE USER*\n\nTag user, tulis nomor, atau balas member yang ingin di RESET`, m);
+    }
 
-	if (!text && !m.quoted) return conn.reply(m.chat, `*❏ DELETE USER*\n\nTag user, tulis nomor, atau balas member yang ingin di RESET`, m)
-	//let exists = await conn.isOnWhatsApp(number)
-	// if (exists) return conn.reply(m.chat, `*Nomor target tidak terdaftar di WhatsApp*`, m)
-	if (isNaN(number)) return conn.reply(m.chat, `*❏ DELETE USER*\n\nNomor yang kamu masukkan tidak valid !`, m)
-	if (number.length > 15) return conn.reply(m.chat, `*❏ DELETE USER*\n\nNomor yang kamu masukkan tidak valid !`, m)
-	try {
-		if (text) {
-			var user = number + '@s.whatsapp.net'
-		} else if (m.quoted.sender) {
-			var user = m.quoted.sender
-		} else if (m.mentionedJid) {
-			var user = number + '@s.whatsapp.net'
-		}
-	} catch (e) {
-	} finally {
+    let deletedUsers = [];
 
-		let groupMetadata = m.isGroup ? await conn.groupMetadata(m.chat) : {}
-		let participants = m.isGroup ? groupMetadata.participants : []
-		let users = m.isGroup ? participants.find(u => u.jid == user) : {}
-		let number = user.split('@')[0]
+    for (let i = 0; i < numbers.length; i++) {
+        let number = numbers[i];
 
-		delete global.db.data.users[user]
+        if (isNaN(number) || number.length > 15) {
+            conn.reply(m.chat, `*❏ DELETE USER*\n\nNomor '${number}' tidak valid!`, m);
+            continue;
+        }
 
-		conn.reply(m.chat, `*❏ DELETE USER*\n\nBerhasil menghapus @${number} dari *DATABASE*`, null, {
-			contextInfo: {
-				mentionedJid: [user]
-			}
-		})
+        let user = number + '@s.whatsapp.net';
+        let groupMetadata = m.isGroup ? await conn.groupMetadata(m.chat) : {};
+        let participants = m.isGroup ? groupMetadata.participants : [];
+        let users = m.isGroup ? participants.find(u => u.jid == user) : {};
 
+        if (users) {
+            delete global.db.data.users[user];
+            deletedUsers.push(`@${number}`);
+        } else {
+            conn.reply(m.chat, `*❏ DELETE USER*\n\nUser dengan nomor @${number} tidak ditemukan di grup ini!`, m);
+        }
+    }
 
-	}
+    if (deletedUsers.length > 0) {
+        conn.reply(m.chat, `*❏ DELETE USER*\n\nBerhasil menghapus ${deletedUsers.join(', ')} dari *DATABASE*`, null, {
+            contextInfo: {
+                mentionedJid: deletedUsers.map(u => u + '@s.whatsapp.net')
+            }
+        });
+    }
 }
 
 handler.help = ['deleteuser']
 handler.tags = ['owner']
 handler.command = /^(d(el)?(ete)?u(ser)?|ha?pu?su(ser)?)$/i
-
 handler.owner = true
 
 export default handler

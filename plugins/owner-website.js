98417d205a4c874d7d54beaa72a07b09349027ad
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-12-25 13:53:17 +0700

    suka suka

--- plugins/owner-website.js
+++ plugins/owner-website.js
@@ -1,53 +1,62 @@
-import fetch from 'node-fetch'
-import { parseString } from 'xml2js'
+import axios from 'axios'
+import { parseStringPromise } from 'xml2js'
+
+// Create a simple cache object
+const cache = {};
 
 let handler = async (m, { conn, text }) => {
   // Extract the option from the command
-  let option = text.toLowerCase(); // Convert to lowercase for case-insensitive comparison
+  let option = text.toLowerCase();
 
   try {
-    // Fetch the sitemap XML
-    const response = await fetch('https://app.ryzendesu.com/sitemap.xml');
-    const xmlData = await response.text();
-
-    // Parse the XML data
-    parseString(xmlData, (err, result) => {
-      if (err) {
-        throw err;
+    // Check if the sitemap is already in the cache
+    if (!cache.sitemap) {
+      // Fetch the sitemap
+      const response = await axios.get('https://app.ryzendesu.com/sitemap.xml');
+      if (response.status !== 200) {
+        throw new Error(`Failed to fetch sitemap. Status: ${response.status}`);
       }
 
-      // Initialize a variable to store the reply message
-      let replyMessage = '';
+      // Store the sitemap in the cache
+      cache.sitemap = response.data;
+    }
 
-      // Use a switch statement to handle different options
-      switch (option) {
-        case 'spotify':
-          const spotifyLink = result.urlset.url.find(
-            (url) => url.loc[0].includes('spotify')
-          );
-          if (spotifyLink) {
-            replyMessage = `Link for Spotify page: ${spotifyLink.loc[0]}`;
-            // Add your specific code for handling the Spotify link here
-          } else {
-            replyMessage = 'Spotify link not found in the sitemap.';
-          }
-          break;
+    // Parse the XML data asynchronously
+    const result = await parseStringPromise(cache.sitemap);
 
-        case 'revanced':
-          const revancedLink = result.urlset.url.find(
-            (url) => url.loc[0].includes('revanced')
-          );
-          if (revancedLink) {
-            replyMessage = `Link for Revanced page: ${revancedLink.loc[0]}`;
-            // Add your specific code for handling the Revanced link here
-          } else {
-            replyMessage = 'Revanced link not found in the sitemap.';
-          }
-          break;
+    // Initialize a variable to store the reply message
+    let replyMessage = '';
 
-        default:
-          // Code to execute when no valid option is provided
-          replyMessage = `
+    // Use a switch statement to handle different options
+    switch (option) {
+      case 'spotify':
+        const spotifyLink = result.urlset.url.find(
+          (url) => url.loc[0].includes('spotify')
+        );
+        if (spotifyLink) {
+          replyMessage = `Link for Spotify page: ${spotifyLink.loc[0]}`;
+        } else {
+          replyMessage = 'Spotify link not found in the sitemap.';
+        }
+        break;
+
+      case 'revanced':
+        const revancedLinks = result.urlset.url.filter(
+          (url) => url.loc[0].includes('revanced')
+        );
+        if (revancedLinks.length > 0) {
+          replyMessage = 'Links for Revanced pages:\n';
+          revancedLinks.forEach((revancedLink) => {
+            replyMessage += `${revancedLink.loc[0]}\n`;
+          });
+        } else {
+          replyMessage = 'Revanced links not found in the sitemap.';
+        }
+        break;
+
+      default:
+        // Code to execute when no valid option is provided
+        replyMessage = `
 *「 Owner Website 」*
 
 Main Site: ryzendesu.com
@@ -55,13 +64,12 @@ Mod APK: app.ryzendesu.com
 KMS Activator: kms.ryzendesu.com
 
 Option: revanced / spotify
-          `;
-          break;
-      }
+        `;
+        break;
+    }
 
-      // Reply with the final message
-      m.reply(replyMessage);
-    });
+    // Reply with the final message
+    m.reply(replyMessage);
   } catch (error) {
     console.error('Error:', error);
     m.reply('Error fetching or parsing the sitemap.');
@@ -71,7 +79,7 @@ Option: revanced / spotify
 handler.help = ['website <option>']
 handler.tags = ['main']
 handler.command = /^(website)$/i
+
 handler.limit = false
 
 export default handler
-

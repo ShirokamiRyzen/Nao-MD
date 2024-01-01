8bb04639492df396d96c6e18ecf8ad4e799f34e0
ShirokamiRyzen <infinixgaming360@gmail.com>
2024-01-01 21:38:36 +0700

    modified:   plugins/_antitoxic.js

--- plugins/_antitoxic.js
+++ plugins/_antitoxic.js
@@ -1,4 +1,4 @@
-let badwordRegex = /anj(k|g)|ajn?(g|k)|a?njin(g|k)|bajingan|b(a?n)?gsa?t|ko?nto?l|me?me?(k|q)|pe?pe?(k|q)|meki|titi(t|d)|pe?ler|tetek|toket|ngewe|go?blo?k|to?lo?l|idiot|(k|ng)e?nto?(t|d)|jembut|bego|dajj?al|janc(u|o)k|pantek|puki ?(mak)?|kimak|kampang|lonte|col(i|mek?)|pelacur|henceu?t|nigga|fuck|dick|bitch|tits|bastard|asshole/i // tambahin sendiri
+let badwordRegex = /anj(k|g)|ajn?(g|k)|a?njin(g|k)|bajingan|b(a?n)?gsa?t|ko?nto?l|me?me?(k|q)|pe?pe?(k|q)|meki|titi(t|d)|pe?ler|tetek|toket|ngewe|go?blo?k|to?lo?l|idiot|(k|ng)e?nto?(t|d)|jembut|bego|dajj?al|janc(u|o)k|pantek|puki ?(mak)?|kimak|kampang|lonte|col(i|mek?)|pelacur|henceu?t|nigga|fuck|dick|bitch|tits|bastard|asshole|dontol|kontoi|ontol/i // tambahin sendiri
 
 export function before(m, { isBotAdmin }) {
     if (m.isBaileys && m.fromMe) return !0
@@ -15,8 +15,8 @@ export function before(m, { isBotAdmin }) {
             user.warning = 0
             if (m.isGroup) {
                 if (isBotAdmin) {
-                	this.groupParticipantsUpdate(m.chat, [m.sender], "remove")
-                   //this.groupSettingChange(m.chat, GroupSettingChange.messageSend, false)
+                    this.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+                    //this.groupSettingChange(m.chat, GroupSettingChange.messageSend, false)
                 }
             }
         }

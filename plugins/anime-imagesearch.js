5bf4863365535923fce416ccfe1e7ceb1c2e01f6
ShirokamiRyzen <infinixgaming360@gmail.com>
2024-01-31 12:20:24 +0700

    modified:   plugins/anime-imagesearch.js

--- plugins/anime-imagesearch.js
+++ plugins/anime-imagesearch.js
@@ -8,10 +8,10 @@ let handler = async (m, { conn }) => {
     try {
         let q = m.quoted ? m.quoted : m;
         let mime = (q.msg || q).mimetype || '';
-        if (!mime) throw 'Kirim/Reply Gambar dengan caption .anime?';
-        
+        if (!mime) throw 'Kirim/Reply Gambar dengan caption .animesearch';
+
         m.reply(wait);
-        
+
         let media = await q.download();
         let url = await uploadImage(media);
         let hasil = await fetch(`https://api.trace.moe/search?url=${encodeURIComponent(url)}`);
@@ -19,23 +19,28 @@ let handler = async (m, { conn }) => {
 
         if (response && response.result && response.result.length > 0) {
             let firstResult = response.result[0];
-            
+
             let filename = firstResult.filename;
             let episode = firstResult.episode;
             let similarity = firstResult.similarity;
             let videoURL = firstResult.video;
 
             let caption = `Name: ${filename}\nEpisode: ${episode}\n\nSimilarity: ${similarity}`;
-            
+
             await conn.sendFile(m.chat, videoURL, filename, caption, m);
         } else {
             m.reply('No result found');
         }
     } catch (error) {
         console.error(error);
-        m.reply('Internal server error');
+        if (error.includes('Kirim/Reply Gambar dengan caption .animesearch')) {
+            m.reply(error);
+        } else {
+            m.reply('Internal server error');
+        }
     }
 };
+
 
 handler.help = ['animesearch']
 handler.tags = ['anime']

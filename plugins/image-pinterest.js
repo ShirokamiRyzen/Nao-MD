a5671102f5ce9ebc5ec6ed36f1cb41057bd34d17
ShirokamiRyzen <infinixgaming360@gmail.com>
2024-02-08 07:10:26 +0700

    full scrape

--- plugins/image-pinterest.js
+++ plugins/image-pinterest.js
@@ -1,16 +1,15 @@
 //By Shirokami Ryzen
 //Dont delete this credit!!!
 import fetch from 'node-fetch'
+import cheerio from 'cheerio'
 
 let handler = async (m, { conn, command, text, usedPrefix }) => {
-  let api = "api.ryzendesu.vip"
   if (!text) throw `Input *Query*`
   conn.reply(m.chat, 'Wait a moment...', m)
 
   try {
-    let res = await fetch(`https://api.ryzendesu.vip/api/search/pinterest?text=${text}&apikey=${global.ryzen}`);
-    let result = await res.json();
-    let gambarUrls = result.result.slice(0, 20); // Ambil 20 gambar pertama
+    const hasil = await pinterest(text);
+    let gambarUrls = hasil.slice(0, 20); // Ambil 20 gambar pertama
 
     // Mengacak array gambarUrls
     for (let i = gambarUrls.length - 1; i > 0; i--) {
@@ -30,7 +29,6 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
       // Tambahkan jeda agar tidak mengirim gambar terlalu cepat
       await new Promise(resolve => setTimeout(resolve, 500));
     }
-    m.reply(api)
   } catch (e) {
     console.log(e)
     conn.reply(m.chat, 'Terjadi kesalahan saat mendownload gambar.', m)
@@ -42,3 +40,30 @@ handler.tags = ['internet']
 handler.command = /^pinterest$/i
 
 export default handler
+
+async function pinterest(text) {
+  try {
+    const response = await fetch(`https://id.pinterest.com/search/pins/?autologin=true&q=${encodeURIComponent(text)}`, {
+      headers: {
+        "cookie": "_auth=1; _b=\"AXOtdcLOEbxD+qMFO7SaKFUCRcmtAznLCZY9V3z9tcTqWH7bPo637K4f9xlJCfn3rl4=\"; _pinterest_sess=TWc9PSZWcnpkblM5U1pkNkZ0dzZ6NUc5WDZqZEpGd2pVY3A0Y2VJOGg0a0J0c2JFWVpQalhWeG5iTTRJTmI5R08zZVNhRUZ4SmsvMG1CbjBWUWpLWVFDcWNnNUhYL3NHT1EvN3RBMkFYVUU0T0dIRldqVVBrenVpbGo5Q1lONHRlMzBxQTBjRGFSZnFBcTdDQVgrWVJwM0JtN3VRNEQyeUpsdDYreXpYTktRVjlxb0xNanBodUR1VFN4c2JUek1DajJXbTVuLzNCUDVwMmRlZW5VZVpBeFQ5ZC9oc2RnTGpEMmg4M0Y2N2RJeVo2aGNBYllUYjRnM05VeERzZXVRUVVYNnNyMGpBNUdmQ1dmM2s2M0txUHRuZTBHVFJEMEE1SnIyY2FTTm9DUEVTeWxKb3V0SW13bkV3TldyOUdrdUZaWGpzWmdaT0JlVnhWb29xWTZOTnNVM1NQSzViMkFUTjBpRitRRVMxaUFxMEJqell1bVduTDJid2l3a012RUgxQWhZT1M3STViSVkxV0dSb1p0NTBYcXlqRU5nPT0ma25kRitQYjZJNTVPb2tyVnVxSWlleEdTTkFRPQ==; _ir=0"
+      }
+    });
+    const data = await response.text();
+    const $ = cheerio.load(data);
+    const result = [];
+    const hasil = [];
+    $('div > a').get().map(b => {
+      const link = $(b).find('img').attr('src');
+      result.push(link);
+    });
+    result.forEach(v => {
+      if (v && v.includes('236')) {
+        hasil.push(v.replace(/236/g, '736'));
+      }
+    });
+    hasil.shift();
+    return hasil;
+  } catch (error) {
+    throw error;
+  }
+}

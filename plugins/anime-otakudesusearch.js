6877f94557bb97231cb7b15a5c1b51a4fa217a2a
ShirokamiRyzen <infinixgaming360@gmail.com>
2024-02-18 15:07:56 +0700

    modified:   plugins/anime-otakudesusearch.js

--- plugins/anime-otakudesusearch.js
+++ plugins/anime-otakudesusearch.js
@@ -2,28 +2,26 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text }) => {
   if (!text) throw `Judulnya?`;
-  let res = await fetch(`https://ryzendesu.vip/api/anime/?search=${text}`);
+  let res = await fetch(`https://ryzendesu-backend.vercel.app/anime/?search=${text}`);
   let otaku = await res.json();
 
-  if (!otaku.data || otaku.data.length === 0) {
+  if (!otaku || otaku.length === 0) {
     throw `Anime tidak ditemukan.`;
   }
 
-  // Mengambil nilai id dari JSON
-  let animeId = otaku.data[0].id;
+  // Mengambil slug dari JSON
+  let animeSlug = otaku[0].slug;
 
-  // Membuat URL dengan menggabungkan URL dasar dan id
-  let animeUrl = `https://ryzendesu.vip/${animeId}`;
+  // Membuat URL dengan menggabungkan URL dasar dan slug
+  let animeUrl = `https://www.ryzendesu.vip/anime/${animeSlug}`;
 
-  // Mendapatkan thumbnail URL
-  let thumbnailUrl = otaku.data[0].thumbnail;
+  // Mengambil thumbnail URL
+  let thumbnailUrl = otaku[0].gambar;
 
   // Mengubah thumbnail URL sesuai format yang diinginkan
   thumbnailUrl = `https://external-content.duckduckgo.com/iu/?u=${thumbnailUrl}`;
 
-  let otakuinfo = `• *Title:* ${otaku.data[0].title}
-• *Genre:* ${otaku.data[0].genres}
-
+  let otakuinfo = `• *Title:* ${otaku[0].judul}
 • *Link*: ${animeUrl}`;
 
   conn.sendFile(m.chat, thumbnailUrl, 'otaku.jpeg', otakuinfo, m);

20eb18323cc5f83d54996947252e69e4e50e0d0e
MythEclipse <90584806+Asepharyana71@users.noreply.github.com>
2024-02-24 16:29:21 +0700

    Update sticker-toimg.js
    fix

--- plugins/sticker-toimg.js
+++ plugins/sticker-toimg.js
@@ -1,19 +1,45 @@
-import { webp2png } from '../lib/webp2mp4.js'
+import sharp from 'sharp';
+
+const TIMEOUT = 10000; // 10 detik
+
 let handler = async (m, { conn, usedPrefix, command }) => {
-    const notStickerMessage = `Reply sticker with command *${usedPrefix + command}*`
-    if (!m.quoted) throw notStickerMessage
-    const q = m.quoted || m
-    let mime = q.mediaType || ''
-    if (!/sticker/.test(mime)) throw notStickerMessage
-    let media = await q.download()
-    let out = await webp2png(media).catch(_ => null) || Buffer.alloc(0)
-    await conn.sendFile(m.chat, out, 'out.png', '*DONE (≧ω≦)ゞ*', m)
-}
-handler.help = ['toimg (reply)']
-handler.tags = ['sticker']
-handler.command = ['toimg']
+  const notStickerMessage = `Reply sticker dengan command *${usedPrefix + command}*`;
 
-handler.register = true
-handler.limit = true
+  if (!m.quoted) throw notStickerMessage;
 
-export default handler
+  const q = m.quoted || m;
+  const mime = q.mimetype || '';
+
+  if (!/image\/webp/.test(mime)) throw notStickerMessage;
+
+  try {
+    // Download sticker
+    const media = await q.download();
+
+    // Dekoding WebP tanpa webp-js
+    const decodedBuffer = await sharp(media).toFormat('png').toBuffer();
+
+    // Send PNG image
+    if (decodedBuffer.length > 0) {
+      await conn.sendFile(m.chat, decodedBuffer, 'out.png', '*DONE (≧ω≦)ゞ*', m);
+    } else {
+      throw 'Gagal mengonversi stiker menjadi gambar.';
+    }
+  } catch (error) {
+    console.error(error);
+    if (error.message === 'Timeout of 10000ms exceeded') {
+      m.reply('Proses konversi terlalu lama. Silakan coba lagi.');
+    } else {
+      m.reply(`Terjadi kesalahan: ${error.message}`);
+    }
+  }
+};
+
+handler.help = ['toimg (reply)'];
+handler.tags = ['sticker'];
+handler.command = ['toimg'];
+
+handler.register = true;
+handler.limit = true;
+
+export default handler;

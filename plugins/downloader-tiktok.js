eff01831a04276de18fdc0197a87619770afb548
ShirokamiRyzen <infinixgaming360@gmail.com>
2024-03-28 12:17:32 +0700

    modified:   plugins/downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -29,9 +29,7 @@ var handler = async (m, { conn, args }) => {
         if (videoURL || videoURLWatermark) {
             await conn.sendFile(m.chat, videoURL, 'tiktok.mp4', `Ini kak videonya\n\n${infonya_gan}`, m);
             setTimeout(async () => {
-                //await conn.sendFile(m.chat, videoURLWatermark, 'tiktokwm.mp4', `*Ini Versi Watermark*\n\n${infonya_gan}`, m);
-                //await conn.sendFile(m.chat, `${tiktokData.music.play_url}`,'lagutt.mp3', 'ini lagunya',m);
-            }, 5000);
+            }, 25000);
         } else {
             throw 'Tidak ada tautan video yang tersedia.';
         }
@@ -45,17 +43,13 @@ var handler = async (m, { conn, args }) => {
                 throw 'Gagal mendownload video!';
             }
 
-            const videoURL2 = tiktokData2.video;
-            const audioURL2 = tiktokData2.audio;
-            const thumbnailURL = tiktokData2.thumbnail;
+            const videoURL2 = tiktokData2.video0;
+            //const soundURL2 = tiktokData2.sound;
 
-            // Lakukan apa yang Anda perlukan dengan tiktokData2 dari Server 2 di sini
-            //await conn.sendFile(m.chat, thumbnailURL, 'thumbnail.jpg', 'Ini thumbnail videonya', m);
             await conn.sendFile(m.chat, videoURL2, 'tiktok2.mp4', 'Ini kak videonya dari Server 2', m);
-            //await conn.sendFile(m.chat, audioURL2, 'tiktok.mp3', 'Ini kak audionya', m);
+           // await conn.sendFile(m.chat, soundURL2, 'tiktok_sound.mp3', 'Ini kak soundnya dari Server 2', m);
 
         } catch (error2) {
-            // Jika server kedua juga gagal, tangani error di sini
             conn.reply(m.chat, `Error: ${error2}`, m);
         }
     }
@@ -77,41 +71,32 @@ async function tryServer1(url) {
 }
 
 async function tryServer2(url) {
-    // jika server 1 gagal, gunakan tikdown.org/id
-    const tikdownURL = 'https://tikdown.org/id';
-    const response = await axios.get(tikdownURL);
+    // server 2 
+    try {
+        const response = await axios.get(`https://tikdown.ryzendesu.vip/api/download?mobile=${url}`);
 
-    if (response.status === 200) {
-        const gettoken = await axios.get("https://tikdown.org/id");
-        const $ = cheerio.load(gettoken.data);
-        const token = $("#download-form > input[type=hidden]:nth-child(2)").attr(
-            "value"
-        );
-        const param = {
-            url: url,
-            _token: token,
-        };
-        const {
-            data
-        } = await axios.request("https://tikdown.org/getAjax?", {
-            method: "post",
-            data: new URLSearchParams(Object.entries(param)),
-            headers: {
-                "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-                "user-agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36",
-            },
-        });
-        var getdata = cheerio.load(data.html);
-        if (data.status) {
+        if (response.status === 200) {
+            const data = response.data;
+
+            if (data.video) {
+                return {
+                    video: true,
+                    video0: data.video0,
+                    video1: data.video1,
+                    sound: data.sound,
+                    author: data.author
+                };
+            } else {
+                return {
+                    status: false
+                };
+            }
+        } else {
             return {
-                status: true,
-                thumbnail: getdata("img").attr("src"),
-                video: getdata("div.download-links > div:nth-child(1) > a").attr("href"),
-                audio: getdata("div.download-links > div:nth-child(2) > a").attr("href"),
+                status: false
             };
-        } else
-            return {
-                status: false,
-            };
-    };
+        }
+    } catch (error) {
+        throw error;
+    }
 }

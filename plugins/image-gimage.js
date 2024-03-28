00fa1dc92e2f8e176bc68dd760b02e22d458417d
ShirokamiRyzen <infinixgaming360@gmail.com>
2024-03-28 10:47:39 +0700

    modified:   plugins/image-gimage.js

--- plugins/image-gimage.js
+++ plugins/image-gimage.js
@@ -2,14 +2,20 @@ import { googleImage } from '@bochilteam/scraper'
 
 var handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!text) throw `Use example ${usedPrefix}${command} Minecraft`
+    
+    if (/(hentai|lewd|nude|bokep|porn|sex|furry|bugil|pussy|telanjang)/i.test(text) && global.db.data.users[m.sender].role === 'Free user') {
+        return conn.reply(m.chat, 'Hayoo mau ngapain lu?\n\nPesan ini otomatis diteruskan ke owner', m)
+    }
+    
     const res = await googleImage(text)
     let image = res.getRandom()
     let link = image
     conn.sendFile(m.chat, link, 'google.jpg', `*${htki} Google Image ${htka}*
 🔎 *Result:* ${text}
 🌎 *Source:* Google
-`,m)
+`, m)
 }
+
 handler.help = ['gimage <query>', 'image <query>']
 handler.tags = ['internet']
 handler.command = /^(gimage|image)$/i

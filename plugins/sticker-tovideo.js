66748447e7eba57aed46d04fe060b44fea725bc8
ShirokamiRyzen <infinixgaming360@gmail.com>
2024-04-01 17:19:02 +0700

    modified:   plugins/sticker-tovideo.js

--- plugins/sticker-tovideo.js
+++ plugins/sticker-tovideo.js
@@ -1,24 +1,17 @@
-import { webp2mp4 } from '../lib/webp2mp4.js'
-import { ffmpeg } from '../lib/converter.js'
-let handler = async (m, { conn, usedPrefix, command }) => {
-    if (!m.quoted) throw `Balas stiker/audio yang ingin diubah menjadi video dengan perintah ${usedPrefix + command}`
-    let mime = m.quoted.mimetype || ''
-    if (!/webp|audio/.test(mime)) throw `Balas stiker/audio yang ingin diubah menjadi video dengan perintah ${usedPrefix + command}`
-    let media = await m.quoted.download()
-    let out = Buffer.alloc(0)
-    if (/webp/.test(mime)) {
-        out = await webp2mp4(media)
-    } else if (/audio/.test(mime)) {
-        out = await ffmpeg(media, [
-            '-filter_complex', 'color',
-            '-pix_fmt', 'yuv420p',
-            '-crf', '51',
-            '-c:a', 'copy',
-            '-shortest'
-        ], 'mp3', 'mp4')
-    }
-    await conn.sendFile(m.chat, out, 'out.mp4', '*DONE*', m, 0, { thumbnail: out })
+import * as fs from 'fs'
+import { exec } from 'child_process'
+
+let handler = async (m, { conn }) => {
+	if (m.quoted && /sticker/.test(m.quoted.mtype) && !m.quoted.isAnimated) {
+		let img = await m.quoted.download()
+		await conn.sendMessage(m.chat, { image: img, jpegThumbnail: img }, { quoted: m })
+	} else if (m.quoted && /sticker/.test(m.quoted.mtype) && m.quoted.isAnimated) {
+		let img = await m.quoted.download()
+		let out = await webpToVideo(img)
+		await conn.sendMessage(m.chat, { video: out, gifPlayback: /gif/i.test(m.text), gifAttribution: ~~(Math.random() * 2) }, { quoted: m })
+	} else throw 'Reply a sticker!'
 }
+
 handler.help = ['tovideo']
 handler.tags = ['sticker']
 
@@ -28,3 +21,28 @@ handler.register = true
 handler.limit = 2
 
 export default handler
+
+function webpToVideo(bufferImage) {
+	return new Promise((resolve, reject) => {
+		try {
+			let pathFile = "./tmp/" + ~~(Math.random() * 1000000 + 1) + ".webp"
+			fs.writeFileSync(pathFile, bufferImage)
+			exec(`convert ${pathFile} ${pathFile}.gif`, (error, stdout, stderr) => {
+				exec(`ffmpeg -i ${pathFile}.gif -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" ${pathFile}.mp4`, (error, stdout, stderr) => {
+					if (!fs.existsSync(pathFile + ".gif") || !fs.existsSync(pathFile + ".mp4")) {
+						reject("Failed convert file!")
+						fs.unlinkSync(pathFile)
+						return
+					}
+					let videoBuffer = fs.readFileSync(pathFile + ".mp4")
+					fs.unlinkSync(pathFile)
+					fs.unlinkSync(pathFile + ".gif")
+					fs.unlinkSync(pathFile + ".mp4")
+					resolve(videoBuffer)
+				})
+			})
+		} catch(e) {
+			reject(e)
+		}
+	})
+}

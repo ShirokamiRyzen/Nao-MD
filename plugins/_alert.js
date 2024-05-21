ed23740fdf065ab990f2005908216c4a4d116f10
ShirokamiRyzen <infinixgaming360@gmail.com>
2024-05-21 11:43:44 +0700

    ya

--- plugins/_alert.js
+++ plugins/_alert.js
@@ -1,4 +1,4 @@
-let handler = m => m
+let handler = m => m;
 
 handler.before = async function (m) {
     // Check if the message is sent in a group
@@ -8,11 +8,19 @@ handler.before = async function (m) {
     }
 
     // Check if the user is banned
-    if (global.db.data.users[m.sender].banned === true) {
-        let banReason = global.db.data.users[m.sender].banReason || 'No reason provided.';
-        m.reply(`Sorry, your number has been blocked from using this bot.\n\nReason: ${banReason}`);
+    let user = global.db.data.users[m.sender];
+    if (user.banned === true) {
+        // Get the current timestamp
+        let now = Date.now();
+        // Check if the user has been notified in the last 24 hours (86400000 milliseconds)
+        if (!user.lastNotified || now - user.lastNotified > 86400000) {
+            // Update the last notified timestamp
+            user.lastNotified = now;
+            let banReason = user.banReason || 'No reason provided.';
+            m.reply(`Sorry, your number has been blocked from using this bot.\n\nReason: ${banReason}`);
+        }
         return;
     }
 }
 
-export default handler
+export default handler;

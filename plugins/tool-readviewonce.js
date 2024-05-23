785c993b5fed8789111ad96915f67296e6ba5a96
ShirokamiRyzen <infinixgaming360@gmail.com>
2024-05-24 00:07:24 +0700

    modified:   plugins/tool-readviewonce.js

--- plugins/tool-readviewonce.js
+++ plugins/tool-readviewonce.js
@@ -1,9 +1,15 @@
 var handler = async (m, { conn }) => {
     if (!/viewOnce/.test(m.quoted?.mtype)) throw 'Itu Bukan Pesan ViewOnce'
-	let mtype = Object.keys(m.quoted.message)[0]
-	let buffer = await m.quoted.download()
-	let caption = m.quoted.message[mtype].caption || ''
-	conn.sendMessage(m.chat, { [mtype.replace(/Message/, '')]: buffer, caption }, { quoted: m })
+    let mtype = Object.keys(m.quoted.message)[0]
+    let buffer = await m.quoted.download()
+    let caption = m.quoted.message[mtype].caption || ''
+    
+    // Memeriksa apakah kata kunci "admin" ada dalam teks pesan
+    if (caption.toLowerCase().includes('×')) {
+        throw 'Not allowed by user'
+    }
+    
+    conn.sendMessage(m.chat, { [mtype.replace(/Message/, '')]: buffer, caption }, { quoted: m })
 }
 
 handler.help = ['read']

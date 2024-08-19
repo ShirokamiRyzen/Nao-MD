2b015d9baaabf938f24fa0531db6daec0c04fb24
ShirokamiRyzen <ryzen@ryzendesu.vip>
2024-08-19 18:44:03 +0700

    modified:   plugins/sticker-qc.js

--- plugins/sticker-qc.js
+++ plugins/sticker-qc.js
@@ -1,59 +1,29 @@
 import { sticker } from '../lib/sticker.js'
 import axios from 'axios'
 
-const handler = async (m, { conn, args }) => {
-    let text;
+const handler = async (m, { conn, args, usedPrefix, command }) => {
+    let text
     if (args.length >= 1) {
         text = args.slice(0).join(" ");
     } else if (m.quoted && m.quoted.text) {
-        text = m.quoted.text;
-    } else throw "Input teks atau reply teks yang ingin dijadikan quote!";
-    if (!text) return m.reply('masukan text');
-    if (text.length > 100) return m.reply('Maksimal 100 Teks!');
+        text = m.quoted.text
+    } else throw "Mana teksnya?"
+    if (!text) return m.reply('Mana teksnya?')
 
-    const randomColor = ['#000000'];
-
-    const apiColor = randomColor[Math.floor(Math.random() * randomColor.length)];
-
-    const pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/320b066dc81928b782c7b.png');
-
-    const obj = {
-        "type": "quote",
-        "format": "png",
-        "backgroundColor": apiColor,
-        "width": 512,
-        "height": 768,
-        "scale": 2,
-        "messages": [{
-            "entities": [],
-            "avatar": true,
-            "from": {
-                "id": 1,
-                "name": m.name,
-                "photo": {
-                    "url": pp
-                }
-            },
-            "text": text,
-            "replyMessage": {}
-        }]
-    };
-
-    const json = await axios.post('https://quote.btch.bz/generate', obj, {
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
-
+    const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
+    const mentionRegex = new RegExp(`@${who.split('@')[0].replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}\\s*`, 'g');
+    const orang = text.replace(mentionRegex, '');
+    const pp = await conn.profilePictureUrl(who).catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
+    const number = await conn.getName(who)
+    const obj = { "type": "quote", "format": "png", "backgroundColor": "#000000", "width": 1024, "height": 1024, "scale": 2, "messages": [{ "entities": [], "avatar": true, "from": { "id": 1, "name": `${who?.name || number}`, "photo": { url: `${pp}` } }, "text": orang, "replyMessage": {} }] };
+    const json = await axios.post('https://bot.lyo.su/quote/generate', obj, { headers: { 'Content-Type': 'application/json' } });
     const buffer = Buffer.from(json.data.result.image, 'base64');
-    const stiker = await sticker(buffer, false, global.stickpack, global.stickauth);
-    if (stiker) return conn.sendFile(m.chat, stiker, 'Quotely.webp', '', m);
+    let stiker = await sticker(buffer, false, global.stickpack, global.stickauth);
+    if (stiker) return conn.sendFile(m.chat, stiker, 'qc.webp', '', m);
 }
 
 handler.help = ['qc']
 handler.tags = ['sticker']
 handler.command = /^(qc)$/i
-
-handler.register = true
 
 export default handler

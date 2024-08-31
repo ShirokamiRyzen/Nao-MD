77ac3d297a511a5c44f503799ac01b73653b02e5
ShirokamiRyzen <ryzen@ryzendesu.vip>
2024-08-31 08:35:04 +0700

    modified:   plugins/ai-animediff.js

--- plugins/ai-animediff.js
+++ plugins/ai-animediff.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 
     await conn.relayMessage(m.chat, { reactionMessage: { key: m.key, text: '👌' } }, { messageId: m.key.id })
     try {
-        let url = `https://api.ryzendesu.vip/api/ai/flux-diffusion?prompt=${text}`
+        let url = `https://api.ryzendesu.vip/api/ai/flux-schnell?prompt=${text}`
 
         await conn.sendFile(m.chat, await (await fetch(url)).buffer(), 'fubuki.jpg', wm, m)
         m.react(done)

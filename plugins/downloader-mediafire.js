c4bc8c2b717f3d9f90a73278725de748609d9219
ShirokamiRyzen <ryzen@ryzendesu.vip>
2024-12-18 18:56:58 +0700

    modified:   plugins/downloader-mediafire.js

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -1,20 +1,52 @@
-import { mediafiredl } from '@bochilteam/scraper'
+import axios from 'axios'
+
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.mediafire.com/file/941xczxhn27qbby/GBWA_V12.25FF-By.SamMods-.apk/file`
-    let res = await mediafiredl(args[0])
-    let { url, url2, filename, ext, aploud, filesize, filesizeH } = res
-    let caption = `
+    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.mediafire.com/file/in5j3u2zwoq1x33/BLUR_BLUR_ASIK.zip/file`;
+    
+    let mediafireApi = `https://api.ryzendesu.vip/api/downloader/mediafire?url=${encodeURIComponent(args[0])}`;
+
+    m.reply(wait)
+
+    try {
+        let res = await axios.get(mediafireApi);
+        let data = res.data;
+
+        if (!data || !data.download) throw 'Failed to fetch download link. Please try again later.';
+
+        let { filename, size, mimetype: ext, created, download, cookie } = data;
+
+        let caption = `
 *💌 Name:* ${filename}
-*📊 Size:* ${filesizeH}
+*📊 Size:* ${size}
 *🗂️ Extension:* ${ext}
-*📨 Uploaded:* ${aploud}
-`.trim()
-    m.reply(caption)
-    await conn.sendFile(m.chat, url, filename, '', m, null, { mimetype: ext, asDocument: true })
-}
-handler.help = ['mediafire'].map(v => v + ' <url>')
-handler.tags = ['downloader']
-handler.command = /^(mediafire|mf)$/i
+*📨 Uploaded:* ${new Date(created).toLocaleString()}
+`.trim();
+
+        let fileRes = await axios.get(download, {
+            headers: {
+                Cookie: cookie
+            },
+            responseType: 'arraybuffer'
+        });
+
+        m.reply(caption);
+        await conn.sendFile(
+            m.chat, 
+            Buffer.from(fileRes.data), 
+            filename, 
+            '', 
+            m, 
+            null, 
+            { mimetype: ext, asDocument: true }
+        );
+    } catch (e) {
+        throw 'An error occurred: ' + e;
+    }
+};
+
+handler.help = ['mediafire'].map(v => v + ' <url>');
+handler.tags = ['downloader'];
+handler.command = /^(mediafire|mf)$/i;
 
 handler.limit = true
 handler.register = true

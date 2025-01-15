090ba41c7fb9d1d153bdf02b0079db79065e006b
ShirokamiRyzen <ryzen@ryzendesu.vip>
2025-01-15 22:54:29 +0700

    test fix with DeepSeek AI

--- plugins/sticker-emojimix.js
+++ plugins/sticker-emojimix.js
@@ -1,28 +1,26 @@
-import MessageType from '@adiwajshing/baileys'
 import fetch from 'node-fetch'
 import { sticker } from '../lib/sticker.js'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
   if (!text) throw `*⛌ Masukan Emoji Yg ingin kamu gabungkan*\n\n*• Example:*\n- ${usedPrefix + command} 😂+😂\n- ${usedPrefix + command} 😂 😂\n\n[ minimal 2 emoji ]`;
 
-  let emojis = text.split(/[\+\s]/).filter(Boolean); // Memisahkan emoji berdasarkan '+' atau spasi
+  let emojis = text.split(/[\+\s]/).filter(Boolean);
   if (emojis.length < 2) throw 'Masukkan minimal 2 emoji untuk di-mix';
-
   if (emojis.length > 2) throw 'Max 2 emoji untuk di-mix';
 
   const anu = await (await fetch(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emojis.join('_'))}`)).json();
 
-  if (anu.results[0] == undefined) throw 'Kombinasi Emojimix Tidak Ditemukan';
+  if (!anu.results[0]) throw 'Kombinasi Emojimix Tidak Ditemukan';
 
   let emix = anu.results[0].media_formats.png_transparent.url;
-  let stiker = await sticker(false, emix, global.stickpack, global.stickauth);
-  conn.sendFile(m.chat, stiker, null, { asSticker: true }, m);
-}
+  const imgBuffer = await (await fetch(emix)).buffer();
+  let stiker = await sticker(imgBuffer, null, global.stickpack, global.stickauth);
+  conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, { asSticker: true });
+};
 
 handler.help = ['emojimix']
 handler.tags = ['sticker']
 handler.command = /^(emojimix|emix)$/i
-
 handler.register = true
 
 export default handler
--- plugins/sticker-meme.js
+++ plugins/sticker-meme.js
@@ -1,5 +1,6 @@
 import { uploadPomf } from '../lib/uploadImage.js'
 import { sticker } from '../lib/sticker.js'
+
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     let [atas, bawah] = text.split`|`
     let q = m.quoted ? m.quoted : m
@@ -8,10 +9,12 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!/image\/(jpe?g|png)/.test(mime)) throw `_*Mime ${mime} tidak didukung!*_`
     let img = await q.download()
     let url = await uploadPomf(img)
-    let meme = `https://api.memegen.link/images/custom/${encodeURIComponent(atas ? atas : '')}/${encodeURIComponent(bawah ? bawah : '')}.png?background=${url}`
-    let stiker = await sticker(false, meme, global.stickpack, global.wm)
-    if (stiker) await conn.sendFile(m.chat, stiker, '', m, '', { asSticker: 1 })
+    let meme = `https://api.memegen.link/images/custom/${encodeURIComponent(atas ? atas : '')}/${encodeURIComponent(bawah ? bawah : '')}.png?background=${url}`;
+    const memeBuffer = await (await fetch(meme)).buffer();
+    let stiker = await sticker(memeBuffer, undefined, global.stickpack, global.wm);
+    if (stiker) await conn.sendFile(m.chat, stiker, '', m, '', { asSticker: 1 });
 }
+
 handler.help = ['smeme <teks atas>|<teks bawah>']
 handler.tags = ['tools']
 handler.command = /^(smeme)$/i

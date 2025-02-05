dfd2a6e0740cd6645f3dde9b33a250dc911e45cc
ShirokamiRyzen <fatihfirdaus404@gmail.com>
2025-02-05 21:12:00 +0700

    modified:   plugins/owner-exec2.js

--- plugins/owner-exec2.js
+++ plugins/owner-exec2.js
@@ -1,54 +1,29 @@
-import syntaxerror from 'syntax-error'
-import { format } from 'util'
-import { fileURLToPath } from 'url'
-import { dirname } from 'path'
-import { createRequire } from 'module'
-
-const __dirname = dirname(fileURLToPath(import.meta.url))
-const require = createRequire(__dirname)
-
-let handler = async (m, _2) => {
-  let { conn, usedPrefix, noPrefix, args, groupMetadata } = _2
-  let _return
-  let _syntax = ''
-  let _text = (/^=/.test(usedPrefix) ? 'return ' : '') + noPrefix
-  let old = m.exp * 1
-  try {
-    let i = 15
-    let f = {
-      exports: {}
-    }
-    let exec = new (async () => { }).constructor('print', 'm', 'handler', 'require', 'conn', 'Array', 'process', 'args', 'groupMetadata', 'module', 'exports', 'argument', _text)
-    _return = await exec.call(conn, (...args) => {
-      if (--i < 1) return
-      console.log(...args)
-      return conn.reply(m.chat, format(...args), m)
-    }, m, handler, require, conn, CustomArray, process, args, groupMetadata, f, f.exports, [conn, _2])
-  } catch (e) {
-    let err = syntaxerror(_text, 'Execution Function', {
-      allowReturnOutsideFunction: true,
-      allowAwaitOutsideFunction: true,
-        sourceType: 'module'
-    })
-    if (err) _syntax = '```' + err + '```\n\n'
-    _return = e
-  } finally {
-    conn.reply(m.chat, _syntax + format(_return), m)
-    m.exp = old
+export const handler = async (m, { conn, text }) => {
+  if (!text) {
+    await conn.reply(m.chat, "Contoh penggunaan: !eval 2 + 2", m);
+    return;
   }
-}
-handler.help = ['> ', '=> ']
-handler.tags = ['advanced']
-handler.customPrefix = /^=?> /
-handler.command = /(?:)/i
 
-handler.rowner = true
+  let output = "";
+  const originalConsoleLog = console.log;
+  console.log = (...args) => {
+    output += args.join(" ") + "\n";
+  };
+
+  try {
+    const result = eval(text);
+    if (typeof result !== "undefined") {
+      output += `Result: ${result}`;
+    }
+  } catch (e) {
+    output += `Error: ${e.toString()}`;
+  }
+  console.log = originalConsoleLog;
+  await m.reply(output.trim(), false, false, { smlcap: false });
+};
+
+handler.command = /^eval|js$/i
+
+handler.owner = true
 
 export default handler
-
-class CustomArray extends Array {
-  constructor(...args) {
-    if (typeof args[0] == 'number') return super(Math.min(args[0], 10000))
-    else return super(...args)
-  }
-}

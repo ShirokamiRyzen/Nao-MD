1dce986e1448b744f12f2dfc3390f5a2c3852daa
ShirokamiRyzen <fatihfirdaus404@gmail.com>
2025-03-05 07:55:40 +0700

    new file:   plugins/stalk-ff.js

---
+++ plugins/stalk-ff.js
@@ -0,0 +1,72 @@
+import axios from 'axios'
+
+let handler = async (m, { conn, args }) => {
+    const userId = args[0]
+
+    if (!userId) throw 'Masukkan User ID'
+
+    let { key } = await conn.sendMessage(m.chat, {
+        text: "Sedang mengecek data akun...",
+    })
+
+    try {
+        let res = await axios.get(`${APIs.ryzen}/api/stalk/ff?userId=${userId}`)
+        let result = res.data
+
+        if (!result.name) throw 'API tidak mengembalikan data yang valid'
+
+        let equippedItemsText = ''
+        if (result.equippedItems && Array.isArray(result.equippedItems)) {
+            equippedItemsText = result.equippedItems
+                .map((item, index) => `${index + 1}. ${item.name}`)
+                .join('\n')
+        }
+
+        let ini_text = `
+*MAIN INFORMATIONS*
+> Name: ${result.name}
+> Bio: ${result.bio}
+> Like: ${result.like}
+> Level: ${result.level}
+> Exp: ${result.exp}
+> Region: ${result.region}
+> Honor Score: ${result.honorScore}
+> BR Rank: ${result.brRank} (${result.brRankPoint})
+> CS Rank Point: ${result.csRankPoint}
+> Account Created: ${result.accountCreated}
+> Last Login: ${result.lastLogin}
+> Prefer Mode: ${result.preferMode}
+> Language: ${result.language}
+> Booyah Pass Level: ${result.booyahPassLevel}
+
+*PET INFORMATION*
+> Name: ${result.petInformation.name}
+> Level: ${result.petInformation.level}
+> Exp: ${result.petInformation.exp}
+> Star Marked: ${result.petInformation.starMarked}
+> Selected: ${result.petInformation.selected}
+
+*EQUIPPED ITEMS*
+${equippedItemsText}
+        `
+
+        await conn.sendMessage(m.chat, {
+            text: ini_text,
+            edit: key
+        })
+    } catch (e) {
+        await conn.sendMessage(m.chat, {
+            text: `error from API: ${e}`,
+            edit: key
+        })
+    }
+}
+
+handler.help = ['ffstalk']
+handler.tags = ['stalk']
+handler.command = /^(stalkff|ffstalk)$/i
+
+handler.register = true
+handler.limit = true
+
+export default handler
--- plugins/stalk-ml.js
+++ plugins/stalk-ml.js
@@ -10,6 +10,56 @@ let handler = async (m, { conn, args }) => {
     if (!userId && !zoneId) throw 'Masukkan User ID & Server ID'
 
     let { key } = await conn.sendMessage(m.chat, {
+        text: "Sedang mengecek data akun...",
+    });
+
+    try {
+        let res = await axios.get(`${APIs.ryzen}/api/stalk/ml?userId=${userId}&zoneId=${zoneId}`)
+        let result = res.data
+
+        if (!result.success) throw 'API tidak mengembalikan data yang valid'
+
+        let ini_text = `
+*RESULT*
+
+> Username: ${result.username}
+> Region: ${result.region}
+`
+        await conn.sendMessage(m.chat, {
+            text: ini_text,
+            edit: key
+        });
+    } catch (e) {
+        await conn.sendMessage(m.chat, {
+            text: `error from API: ${e}`,
+            edit: key
+        });
+    }
+}
+
+handler.help = ['mlstalk']
+handler.tags = ['stalk']
+handler.command = /^(stalkml|mlstalk)$/i
+
+handler.register = true
+handler.limit = true
+
+export default handler
+
+
+/*
+import axios from 'axios'
+
+let handler = async (m, { conn, args }) => {
+
+    const userId = args[0]
+    const zoneId = args[1]
+
+    if (!userId) throw 'Masukkan User ID'
+    if (!zoneId) throw 'Masukkan Server ID'
+    if (!userId && !zoneId) throw 'Masukkan User ID & Server ID'
+
+    let { key } = await conn.sendMessage(m.chat, {
         text: "Ksabar cuy...",
     });
 
@@ -44,10 +94,11 @@ ${result.accountAge.hours} Jam, ${result.accountAge.minutes} Menit, ${result.acc
     }
 }
 
-handler.help = ['cekml <id> <server>']
-handler.tags = ['tools']
-handler.command = /^(cekml|checkml|mlcheck)$/i
+handler.help = ['mlstalk']
+handler.tags = ['stalk']
+handler.command = /^(stalkml|mlstalk)$/i
 
 handler.register = true
 
 export default handler
+*/

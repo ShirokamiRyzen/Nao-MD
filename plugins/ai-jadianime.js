fe03760ee2153e87e9185230f388f2795fefc32c
ShirokamiRyzen <fatihfirdaus404@gmail.com>
2025-03-10 21:29:43 +0700

    To anime

--- plugins/ai-jadianime.js
+++ plugins/ai-jadianime.js
@@ -3,6 +3,9 @@ import { uploadPomf } from '../lib/uploadImage.js'
 
 let handler = async (m, { conn, usedPrefix, command, text }) => {
     try {
+        let args = text.trim().split(/\s+/);
+        let style = args[1] || "anime";
+
         let q = m.quoted ? m.quoted : m;
         let mime = (q.msg || q).mimetype || '';
         if (!mime) throw `Kirim/Reply Gambar dengan caption ${usedPrefix}toanime`;
@@ -11,7 +14,7 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
         let media = await q.download();
         let url = await uploadPomf(media);
 
-        let response = await fetch(`${APIs.ryzen}/api/ai/toanime?url=${url}`);
+        let response = await fetch(`${APIs.ryzen}/api/ai/toanime?url=${url}&style=${style}`);
         if (!response.ok) throw new Error('Failed to fetch image from API');
         let hasil = await response.buffer();
 
@@ -22,7 +25,7 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
 };
 
 handler.help = ['toanime'];
-handler.tags = ['anime', 'ai'];
+handler.tags = ['ai'];
 handler.command = /^(toanime)$/i;
 
 handler.register = true

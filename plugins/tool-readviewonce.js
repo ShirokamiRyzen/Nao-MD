f69ce75cda31bab353effd23568906d129a3ecb9
Fatih Firdaus <fatihfirdaus404@gmail.com>
2025-04-04 09:12:07 +0700

    Update tool-readviewonce.js

--- plugins/tool-readviewonce.js
+++ plugins/tool-readviewonce.js
@@ -1,21 +1,24 @@
-import { downloadContentFromMessage } from '@adiwajshing/baileys'
-
-let handler = async (m, { conn }) => {
-  if (!m.quoted) throw 'Send/Reply Images with the caption *.readviewonve*'
-  if (m.quoted.mtype !== 'viewOnceMessageV2') throw 'Ini bukan pesan view-once.'
-  let msg = m.quoted.message
-  let type = Object.keys(msg)[0]
-  let media = await downloadContentFromMessage(msg[type], type == 'imageMessage' ? 'image' : 'video')
-  let buffer = Buffer.from([])
-  for await (const chunk of media) {
-    buffer = Buffer.concat([buffer, chunk])
-  }
-  if (/video/.test(type)) {
-    return conn.sendFile(m.chat, buffer, 'media.mp4', msg[type].caption || '', m)
-  } else if (/image/.test(type)) {
-    return conn.sendFile(m.chat, buffer, 'media.jpg', msg[type].caption || '', m)
-  }
-}
+let handler = async(m) => {
+	
+	if (!m.quoted) return m.reply(
+		"Reply gambar/video yang ingin Anda lihat"
+	);
+	let typ = ["image/jpeg", "video/mp4"]
+	let regMedia = typ.includes(m.quoted.mimetype);
+	let view = m.quoted?.viewOnce == true ? true : false
+	
+	if (regMedia && view) {
+		let msg = await m.getQuotedObj()?.message;
+		let type = Object.keys(msg)[0];
+		let media = await m.quoted?.download() || await m.getQuotedObj()
+		.download();
+		if (!media) return m.reply("Media gagal di Eksekusi!")
+	
+		await conn.sendFile(
+		m.chat, media, 'error.mp4', msg[type]?.caption || '', m
+		);
+	} else m.reply("Ini bukan pesan view-once.");
+};
 
 handler.help = ['read']
 handler.tags = ['tools']

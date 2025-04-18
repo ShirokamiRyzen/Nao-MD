0a74593d2ba8e63cfe802d77184a3873d0040ae8
ShirokamiRyzen <fatihfirdaus404@gmail.com>
2025-04-18 13:31:03 +0700

    fixed

--- plugins/info-speedtest.js
+++ plugins/info-speedtest.js
@@ -4,53 +4,14 @@ const exec = promisify(execCallback);
 
 const handler = async (m, { conn }) => {
 
-    m.reply(wait)
+    m.reply(wait);
 
     try {
-        const { stdout, stderr } = await exec('python3 speed.py --share --secure');
+        const { stdout } = await exec('python3 speed.py --share --secure');
 
-        const { download, upload, ping, server, client, timestamp, bytes_sent, bytes_received, share, } = JSON.parse(stdout.match(/\{(.|\n)*\}/)[0]);
-
-        const message = `
-🚀 Download Speed: ${formatSpeed(download)} Mbps
-📤 Upload Speed: ${formatSpeed(upload)} Mbps
-⏱ Ping: ${ping} ms
-
-Server Details:
-  Name: ${server.name}
-  Country: ${server.country}
-  Sponsor: ${server.sponsor}
-
-Client Details:
-  IP: #HIDDEN
-  ISP: ${client.isp}
-  Country: ${client.country}
-
-📅 Timestamp: ${formatTimestamp(timestamp)}
-💾 Bytes Sent: ${formatBytes(bytes_sent)}
-💽 Bytes Received: ${formatBytes(bytes_received)}
-🔗 Share Link: ${share}
-`;
-
-        await conn.reply(m.chat, message, m, {
-            contextInfo: {
-                mentionedJid: [m.sender],
-                externalAdReply: {
-                    body: "SPEEDTEST",
-                    containsAutoReply: true,
-                    mediaType: 1,
-                    mediaUrl: 'https://www.speedtest.net/id',
-                    renderLargerThumbnail: true,
-                    showAdAttribution: true,
-                    sourceUrl: 'https://www.speedtest.net/id',
-                    thumbnail: await conn.resize(share, 350, 200),
-                    thumbnailUrl: share,
-                    title: " O O K L A ",
-                },
-            },
-        });
+        await m.reply(stdout.trim());
     } catch (error) {
-        console.error('Terjadi kesalahan:', error.message);
+        m.reply('Error: ' + error.message);
     }
 };
 
@@ -62,29 +23,3 @@ handler.register = true
 handler.rowner = true
 
 export default handler
-
-function formatSpeed(speed) {
-    return (speed / (1024 * 1024)).toFixed(2);
-}
-
-function formatTimestamp(timestamp) {
-    const date = new Date(timestamp);
-    const options = {
-        weekday: 'long',
-        day: 'numeric',
-        month: 'long',
-        year: 'numeric',
-        hour: 'numeric',
-        minute: 'numeric',
-        second: 'numeric',
-        timeZoneName: 'short',
-    };
-    return new Intl.DateTimeFormat('en-US', options).format(date);
-}
-
-function formatBytes(bytes) {
-    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-    if (bytes == 0) return '0 Byte';
-    const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
-    return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
-}
--- speed.py
+++ speed.py
@@ -1909,11 +1909,11 @@ def shell():
                         raise
         sys.exit(0)
 
-    printer('*🌁 ᴛᴇsᴛɪɴɢ ғʀᴏᴍ %(isp)s...*\n' % speedtest.config['client'],
+    printer('*Testing from %(isp)s...*\n' % speedtest.config['client'],
             quiet)
 
     if not args.mini:
-        printer('📑 ʀᴇᴛʀɪᴇᴠɪɴɢ speedtest.net sᴇʀᴠᴇʀ ʟɪsᴛ...', quiet)
+        printer('Retrieving server list...', quiet)
         try:
             speedtest.get_servers(servers=args.server, exclude=args.exclude)
         except NoMatchedServers:
@@ -1931,17 +1931,17 @@ def shell():
             )
 
         if args.server and len(args.server) == 1:
-            printer('📚 Retrieving information for the selected server...', quiet)
+            printer('Retrieving information for the selected server...', quiet)
         else:
-            printer('🔍 sᴇʟᴇᴄᴛɪɴɢ ʙᴇsᴛ sᴇʀᴠᴇʀ ʙᴀsᴇᴅ ᴏɴ ᴘɪɴɢ...', quiet)
+            printer('Selecting best server from ping', quiet)
         speedtest.get_best_server()
     elif args.mini:
         speedtest.get_best_server(speedtest.set_mini_server(args.mini))
 
     results = speedtest.results
 
-    printer('\n.........................................................\n🏠 *ʜᴏsᴛᴇᴅ ʙʏ :* %(sponsor)s\n🌍 *ʟᴏᴄᴀᴛɪᴏɴ :* %(name)s [%(d)0.2f km] '
-            '\n⚡ *ᴘɪɴɢ :* %(latency)s ms' % results.server, quiet)
+    printer('\n.........................................................\n*Hosted by:* %(sponsor)s\n*Location:* %(name)s [%(d)0.2f km] '
+            '\n*Ping :* %(latency)s ms' % results.server, quiet)
 
     if args.download:
         printer('', quiet,
@@ -1950,7 +1950,7 @@ def shell():
             callback=callback,
             threads=(None, 1)[args.single]
         )
-        printer('*📥 ᴅᴏᴡɴʟᴏᴀᴅ:* %0.2f M%s/s' %
+        printer('*Download:* %0.2f M%s/s' %
                 ((results.download / 1000.0 / 1000.0) / args.units[1],
                  args.units[0]),
                 quiet)
@@ -1959,11 +1959,11 @@ def shell():
 
     if args.upload:
         speedtest.upload()
-        printer('*📤 ᴜᴘʟᴏᴀᴅ:* %0.2f M%s/s' %
+        printer('*Upload:* %0.2f M%s/s' %
                 ((results.upload / 1000.0 / 1000.0) / args.units[1],
                  args.units[0]),
                 quiet)
-        printer("\n..................................................\n↬ ᴘᴏᴡᴇʀᴇᴅ ʙʏ *sᴘᴇᴇᴅᴛᴇsᴛ Ookla*\n❖ sᴄʀɪᴘᴛ ᴍᴀᴅᴇ ʙʏ *ɪᴍʏᴀɴxɪᴀᴏ|ᴇʟᴀɪɴᴀ-ʙᴏᴛ & Xnuvers007*")
+        printer("\n..................................................\n↬ POWERED BY *Speedtest Ookla*\n❖ Script made by *Shirokami Ryzen*")
     else:
         printer('Skipping upload test', quiet)
 

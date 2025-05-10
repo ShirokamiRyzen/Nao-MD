e279f1fbc436e67b994214274af56a46fb237b26
ShirokamiRyzen <fatihfirdaus404@gmail.com>
2025-05-10 13:24:43 +0700

    modified:   plugins/info-speedtest.js

--- plugins/info-speedtest.js
+++ plugins/info-speedtest.js
@@ -1,14 +1,18 @@
 import { promisify } from 'util'
 import { exec as execCallback } from 'child_process'
+import os from 'os'
+
 const exec = promisify(execCallback);
 
 const handler = async (m, { conn }) => {
-
     m.reply(wait);
 
     try {
-        const { stdout } = await exec('python3 speed.py --share --secure');
+        const isWindows = os.platform() === 'win32';
+        const pythonCmd = isWindows ? 'python' : 'python3';
+        const command = `${pythonCmd} speed.py --share --secure`;
 
+        const { stdout } = await exec(command);
         await m.reply(stdout.trim());
     } catch (error) {
         m.reply('Error: ' + error.message);

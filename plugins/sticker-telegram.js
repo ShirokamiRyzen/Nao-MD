b9835d05c5bd33b32f002e35e0d3ef8fe78bc8b6
ShirokamiRyzen <fatihfirdaus404@gmail.com>
2025-07-11 20:05:39 +0700

    modified:   plugins/sticker-telegram.js

--- plugins/sticker-telegram.js
+++ plugins/sticker-telegram.js
@@ -2,24 +2,30 @@ import fetch from "node-fetch"
 import { sticker } from '../lib/sticker.js'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Example:\n${usedPrefix + command} https://t.me/addstickers/sshaaaaa`
-    if (!args[0].match(/(https:\/\/t.me\/addstickers\/)/gi)) throw `Invalid URL`
+    if (!args[0]) throw `Contoh:\n${usedPrefix + command} https://t.me/addstickers/sshaaaaa`
+    if (!args[0].match(/(https:\/\/t.me\/addstickers\/)/gi)) throw `URL-nya gak valid, Sayang~`
 
     let apiUrl = `https://api.ryzumi.vip/api/image/sticker-tele?url=${encodeURIComponent(args[0])}`
     let res = await fetch(apiUrl)
-    if (!res.ok) throw `Error: ${res.status} ${res.statusText}`
+    if (!res.ok) throw `Err: ${res.status}`
 
     let json = await res.json()
     let stickers = json.stickers?.stickers || []
-    if (!stickers.length) throw `Not found`
+    if (!stickers.length) throw `Sticker not found`
 
     m.reply(`*Total stiker:* ${stickers.length}`)
 
     for (let s of stickers) {
         if (!s.is_animated && s.image_url) {
-            let stiker = await sticker(false, s.image_url, global.stickpack, global.stickauth)
-            if (stiker) await conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, { quoted: m })
-            await delay(1000)
+            try {
+                let imgRes = await fetch(s.image_url)
+                let buffer = await imgRes.buffer()
+                let stiker = await sticker(buffer, false, global.stickpack, global.stickauth)
+                if (stiker) await conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, { quoted: m })
+                await delay(1000)
+            } catch (e) {
+                console.error(`Gagal proses stiker: ${e}`)
+            }
         }
     }
 
@@ -29,6 +35,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 handler.help = ['stickertele']
 handler.tags = ['sticker']
 handler.command = /^(stic?kertele(gram)?)$/i
+
 handler.limit = 15
 handler.register = true
 
